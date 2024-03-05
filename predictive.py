bce090a3807a6e8643c9da98b13bb30eed8387c1
ctirou <102290014+ctirou@users.noreply.github.com>
2024-03-05 16:48:27 +0100

    all trial_types included in predictive 🚀

--- predictive.py
+++ predictive.py
@@ -20,7 +20,9 @@ from config import *
 # stim disp= 500 ms
 # RSI = 750 ms in task
 
-do_pca = True
+do_pca = False
+
+trial_types = ['all', 'pattern', 'random']
 
 data_path = '/Volumes/Ultra_Touch/pred_asrt'
 # data_path = DATA_DIR
@@ -31,61 +33,96 @@ ensure_dir(figures)
 
 mean_score = list()
 
-for subject in subjects:
-    
-    all_epochs = list()
-    all_behavs = list()
-    
-    for epoch_num, epo in enumerate(epochs_list):
+for trial_type in trial_types:
+     
+    for subject in subjects[:1]:
+        
+        all_epochs = list()
+        all_behavs = list()
+        
+        for epoch_num, epo in enumerate(epochs_list):
 
-        behav = pd.read_pickle(op.join(data_path, 'behav', f'{subject}_{epoch_num}.pkl'))
-        epoch_fname = op.join(data_path, "%s/%s_%s_s-epo.fif" % (lock, subject, epoch_num))
-        epoch = mne.read_epochs(epoch_fname, verbose="error")
-        times = epoch.times
-        
-        if do_pca:
-            n_component = 30    
-            pca = UnsupervisedSpatialFilter(PCA(n_component), average=False)
-            pca_data = pca.fit_transform(epoch.get_data())
-            sampling_freq = epoch.info['sfreq']
-            info = mne.create_info(n_component, ch_types='mag', sfreq=sampling_freq)
-            epoch = mne.EpochsArray(pca_data, info=info, events=epoch.events, event_id=epoch.event_id)
-        
-        all_epochs.append(epoch)
-        all_behavs.append(behav)
-    
-    for epoch in all_epochs: # see mne.preprocessing.maxwell_filter to realign the runs to a common head position. On raw data.
-        epoch.info['dev_head_t'] = all_epochs[0].info['dev_head_t']
-    
-    epochs = mne.concatenate_epochs(all_epochs)
-    behav_df = pd.concat(all_behavs)
+            behav = pd.read_pickle(op.join(data_path, 'behav', f'{subject}_{epoch_num}.pkl'))
+            epoch_fname = op.join(data_path, "%s/%s_%s_s-epo.fif" % (lock, subject, epoch_num))
+            epoch = mne.read_epochs(epoch_fname, verbose="error", preload=False)
+            times = epoch.times
             
-    y = np.array(behav_df['positions'])
-    X = epochs.get_data()
+            # keep only pattern or random trials
+            if trial_type == 'pattern':
+                behav = behav[behav['trialtypes'] == 1]
+                epoch = epoch[np.where(behav['trialtypes']==1)[0]]
+            elif trial_type == 'random':
+                behav = behav[behav['trialtypes'] == 1]
+                epoch = epoch[np.where(behav['trialtypes']==1)[0]]
+            else:
+                pass
+            assert len(epoch) == len(behav)
+            
+            if do_pca:
+                n_component = 30    
+                pca = UnsupervisedSpatialFilter(PCA(n_component), average=False)
+                pca_data = pca.fit_transform(epoch.get_data())
+                sampling_freq = epoch.info['sfreq']
+                info = mne.create_info(n_component, ch_types='mag', sfreq=sampling_freq)
+                epoch = mne.EpochsArray(pca_data, info=info, events=epoch.events, event_id=epoch.event_id)
+            
+            all_epochs.append(epoch)
+            all_behavs.append(behav)
         
-    # 1 ---------- Test clasic sliding estimators
-    clf = make_pipeline(StandardScaler(), LogisticRegression(solver='liblinear'))
-    clf = GeneralizingEstimator(clf, n_jobs=-1, scoring='accuracy', verbose=True)
-    # scores = cross_val_multiscore(clf, X, y, cv=KFold(5))
-    # mean_score.append(scores.mean(axis=0))
+        for epoch in all_epochs: # see mne.preprocessing.maxwell_filter to realign the runs to a common head position. On raw data.
+            epoch.info['dev_head_t'] = all_epochs[0].info['dev_head_t']
+        
+        epochs = mne.concatenate_epochs(all_epochs)
+        behav_df = pd.concat(all_behavs)
+                
+        y = np.array(behav_df['positions'])
+        X = epochs.get_data()
+            
+        # 1 ---------- Test clasic sliding estimators
+        clf = make_pipeline(StandardScaler(), LogisticRegression(solver='liblinear'))
+        clf = GeneralizingEstimator(clf, n_jobs=-1, scoring='accuracy', verbose=True)
+        # scores = cross_val_multiscore(clf, X, y, cv=KFold(5))
+        # mean_score.append(scores.mean(axis=0))
 
-    clf.fit(X, y)
-    score = clf.score(X, y)    
-    mean_score.append(score)
+        clf.fit(X, y)
+        score = clf.score(X, y)    
+        mean_score.append(score)
 
-    # mean_score.append(scores.mean(0))
-    res_path_1 = op.join(figures, "big_gen", "noK")
-    ensure_dir(res_path_1)
-    ymin, ymax = -1, 4
+        # mean_score.append(scores.mean(0))
+        res_path_1 = op.join(figures, "big_gen", "noK")
+        ensure_dir(res_path_1)
+        ymin, ymax = -1, 4
+        fig, ax = plt.subplots(1, 1, figsize=(12, 4))
+        im = ax.imshow(
+            score,
+            interpolation="lanczos",
+            origin="lower",
+            cmap="RdBu_r",
+            extent=times[[0, -1, 0, -1]],
+            aspect=0.7)
+        
+        ax.set_xlabel("Testing Time (s)")
+        ax.set_ylabel("Training Time (s)")
+        ax.set_title("Temporal generalization")
+        ax.axvline(0, color="k")
+        ax.axhline(0, color="k")
+        ax.set_ylim(ymin, ymax)
+        cbar = plt.colorbar(im, ax=ax)
+        cbar.set_label("accuracy")
+        fig.savefig(op.join(res_path_1, "%s.png" % (subject)))
+        
+    mean_score = np.array(mean_score)
+    score_f = mean_score.copy().mean(axis=0)
+
     fig, ax = plt.subplots(1, 1, figsize=(12, 4))
     im = ax.imshow(
-        score,
+        score_f,
         interpolation="lanczos",
         origin="lower",
         cmap="RdBu_r",
         extent=times[[0, -1, 0, -1]],
         aspect=0.7)
-    
+
     ax.set_xlabel("Testing Time (s)")
     ax.set_ylabel("Training Time (s)")
     ax.set_title("Temporal generalization")
@@ -94,26 +131,4 @@ for subject in subjects:
     ax.set_ylim(ymin, ymax)
     cbar = plt.colorbar(im, ax=ax)
     cbar.set_label("accuracy")
-    fig.savefig(op.join(res_path_1, "%s.png" % (subject)))
-    
-mean_score = np.array(mean_score)
-score_f = mean_score.copy().mean(axis=0)
-
-fig, ax = plt.subplots(1, 1, figsize=(12, 4))
-im = ax.imshow(
-    score_f,
-    interpolation="lanczos",
-    origin="lower",
-    cmap="RdBu_r",
-    extent=times[[0, -1, 0, -1]],
-    aspect=0.7)
-
-ax.set_xlabel("Testing Time (s)")
-ax.set_ylabel("Training Time (s)")
-ax.set_title("Temporal generalization")
-ax.axvline(0, color="k")
-ax.axhline(0, color="k")
-ax.set_ylim(ymin, ymax)
-cbar = plt.colorbar(im, ax=ax)
-cbar.set_label("accuracy")
-fig.savefig(op.join(res_path_1, "mean.png"))
+    fig.savefig(op.join(res_path_1, "mean.png"))
