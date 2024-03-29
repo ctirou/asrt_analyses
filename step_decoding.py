dda914ee572370ab684ec8d5b1c1536757cd9874
ctirou <102290014+ctirou@users.noreply.github.com>
2024-03-29 17:54:01 +0100

    combined plot in train prac 🧪

--- step_decoding.py
+++ step_decoding.py
@@ -27,14 +27,6 @@ params = "train_on_practice"
 figures = RESULTS_DIR / 'figures' / lock / 'decoding' / params
 ensure_dir(figures)
 
-# practice
-scores_0 = []
-# blocks 
-scores_1 = []
-scores_2 = []
-scores_3 = []
-scores_4 = []
-
 # set-up the classifier and cv structure
 clf = make_pipeline(StandardScaler(), LogisticRegressionCV(max_iter=10000))
 clf = SlidingEstimator(clf, n_jobs=-1, scoring="accuracy", verbose=True)
@@ -43,6 +35,13 @@ cv = StratifiedKFold(folds, shuffle=True)
 for trial_type in trial_types:
     
     ensure_dir(figures / trial_type)
+    # practice
+    scores_0 = []
+    # blocks 
+    scores_1 = []
+    scores_2 = []
+    scores_3 = []
+    scores_4 = []
 
     for subject in subjects:
 
@@ -79,30 +78,30 @@ for trial_type in trial_types:
         # there is only randoms in practice sessions
         for train_0, test_0 in cv.split(X_0, y_0):
             clf.fit(X_0[train_0], y_0[train_0])
-            scores_0.append(np.array(clf.score(X_0[test_0], y_0[test_0])).mean(0))
+            scores_0.append(np.array(clf.score(X_0[test_0], y_0[test_0])))
             
         for i, score_list in zip(range(1, len(all_epo)), [scores_1, scores_2, scores_3, scores_4]):
+            X = all_epo[i].get_data()
+            y = all_beh[i].positions
+            assert len(X) == len(y)
+            # all
             if trial_type == 'all':
-                # all
-                X = all_epo[i].get_data()
-                y = all_beh[i].positions
-                assert len(X) == len(y)
                 for train, test in cv.split(X, y):
-                    score_list.append(np.array(clf.score(X[test], y[test])).mean(0))
+                    score_list.append(np.array(clf.score(X[test], y[test])))
             elif trial_type == 'pattern':
                 # pattern
                 pattern = all_beh[i].trialtypes == 1
                 X_pat = X[pattern]
-                y_pat = y[pattern]
+                y_pat = np.array(y[pattern])
                 for train, test in cv.split(X_pat, y_pat):
-                    score_list.append(np.array(clf.score(X_pat[test], y_pat[test])).mean(0))
+                    score_list.append(np.array(clf.score(X_pat[test], y_pat[test])))
             else:
                 # random
                 random = all_beh[i].trialtypes == 2
                 X_rdm = X[random]
-                y_rdm = y[random]
+                y_rdm = np.array(y[random])
                 for train, test in cv.split(X_rdm, y_rdm):
-                    score_list.append(np.array(clf.score(X_rdm[test], y_rdm[test])).mean(0))
+                    score_list.append(np.array(clf.score(X_rdm[test], y_rdm[test])))
 
     scores_0 = np.array(scores_0)
     scores_1 = np.array(scores_1)
@@ -112,12 +111,29 @@ for trial_type in trial_types:
             
     for score, label in zip([scores_0, scores_1, scores_2, scores_3, scores_4], ['train_prac', 'test_b1', 'test_b2', 'test_b3', 'test_b4']): 
         plt.subplots(1, 1, figsize=(16, 7))
-        plt.plot(times, score.mean(0), label=label)
+        plt.plot(times, score.mean(0).T, label=label)
         pval = decod_stats(score - chance)
         sig = pval < .05
-        plt.fill_between(times, chance, score.mean(0), where=sig, alpha=.3)
+        plt.fill_between(times, chance, score.mean(0).T, where=sig, alpha=.3)
         plt.legend()
+        plt.title("train on practice")
         plt.axhline(y=0.25, ls="dashed", color="k", alpha=.2)
         plt.axvline(x=0, ls="dashed", color="k", alpha=.2)
-        plt.savefig(op.join(figures, trial_type, "f'{label}.png"))
+        plt.savefig(op.join(figures, trial_type, "%s.png" % label))
         plt.close()
+        
+    plt.subplots(1, 1, figsize=(16, 7))
+    plt.plot(times, scores_1.mean(0).T, label='block_1')
+    plt.plot(times, scores_2.mean(0).T, label='block_2')
+    plt.plot(times, scores_3.mean(0).T, label='block_3')
+    plt.plot(times, scores_4.mean(0).T, label='block_4')
+    for score in [scores_0, scores_1, scores_2, scores_3, scores_4]:
+        pval = decod_stats(score - chance)
+        sig = pval < .05
+        plt.fill_between(times, chance, score.mean(0).T, where=sig, alpha=.3)
+    plt.legend()
+    plt.title("train on practice_combined")
+    plt.axhline(y=0.25, ls="dashed", color="k", alpha=.2)
+    plt.axvline(x=0, ls="dashed", color="k", alpha=.2)
+    plt.savefig(op.join(figures, trial_type, "combined.png"))
+    plt.close()
