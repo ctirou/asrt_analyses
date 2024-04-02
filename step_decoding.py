416f3d2974ad7723f7fdcc38f98e5ed5acbfb978
ctirou <102290014+ctirou@users.noreply.github.com>
2024-04-02 12:25:43 +0200

    train on practice and step 🏃

--- step_decoding.py
+++ step_decoding.py
@@ -5,135 +5,75 @@ import mne
 from mne.decoding import CSP
 from mne.decoding import cross_val_multiscore, SlidingEstimator, GeneralizingEstimator
 from sklearn.pipeline import make_pipeline
-from sklearn.model_selection import StratifiedKFold
+from sklearn.model_selection import StratifiedKFold, train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import RidgeCV, Ridge, LogisticRegressionCV, LogisticRegression
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, multilabel_confusion_matrix, accuracy_score
 import matplotlib.pyplot as plt
 from jr.gat import scorer_spearman
 from sklearn.metrics import make_scorer
 from base import *
 from config import *
 import pandas as pd
+from sklearn.metrics import accuracy_score
 
 trial_types = ["all", "pattern", "random"]
 data_path = DATA_DIR
 lock = "stim"
 subject = "sub01"
 subjects = SUBJS
-folds = 10
+folds = 3
 chance = 0.25
+scoring = "accuracy"
 
-params = "train_on_practice"
+params = "step_decoding"
 figures = RESULTS_DIR / 'figures' / lock / 'decoding' / params
 ensure_dir(figures)
 
 # set-up the classifier and cv structure
 clf = make_pipeline(StandardScaler(), LogisticRegressionCV(max_iter=10000))
-clf = SlidingEstimator(clf, n_jobs=-1, scoring="accuracy", verbose=True)
+clf = SlidingEstimator(clf, n_jobs=-1, scoring=scoring, verbose=True)
 cv = StratifiedKFold(folds, shuffle=True)
 
-for trial_type in trial_types:
-    
-    ensure_dir(figures / trial_type)
-    # practice
-    scores_0 = []
-    # blocks 
-    scores_1 = []
-    scores_2 = []
-    scores_3 = []
-    scores_4 = []
+epo_dir = data_path / lock
+epo_fnames = [epo_dir / f"{f}" for f in sorted(os.listdir(epo_dir)) if ".fif" in f and subject in f]
+all_epo = [mne.read_epochs(fname, preload=False, verbose="error") for fname in epo_fnames]
+times = all_epo[0].times
 
-    for subject in subjects:
+beh_dir = data_path / "behav"
+beh_fnames = [beh_dir / f"{f}" for f in sorted(os.listdir(beh_dir)) if ".pkl" in f and subject in f]
+all_beh = [pd.read_pickle(fname).reset_index() for fname in beh_fnames]
 
-        epo_dir = data_path / lock
-        epo_fnames = [epo_dir / f"{f}" for f in sorted(os.listdir(epo_dir)) if ".fif" in f and subject in f]
-        all_epo = [mne.read_epochs(fname, preload=False, verbose="error") for fname in epo_fnames]
-        times = all_epo[0].times
+for epoch in all_epo:  # see mne.preprocessing.maxwell_filter to realign the runs to a common head position. On raw data.
+    epoch.info["dev_head_t"] = all_epo[0].info["dev_head_t"]
 
-        beh_dir = data_path / "behav"
-        beh_fnames = [beh_dir / f"{f}" for f in sorted(os.listdir(beh_dir)) if ".pkl" in f and subject in f]
-        all_beh = [pd.read_pickle(fname).reset_index() for fname in beh_fnames]
+beh = pd.concat(all_beh)
+epochs = mne.concatenate_epochs(all_epo)
 
-        for epoch in all_epo:  # see mne.preprocessing.maxwell_filter to realign the runs to a common head position. On raw data.
-            epoch.info["dev_head_t"] = all_epo[0].info["dev_head_t"]
+X_0 = all_epo[0].get_data()
+y_0 = np.array(all_beh[0].positions)
 
-        # train on practice data
-        X_0 = mne.read_epochs(epo_fnames[0], preload=True, verbose="error").get_data()
-        y_0 = all_beh[0].positions
-        assert X_0.shape[0] == y_0.shape[0]
-        # scores = cross_val_multiscore(clf, X, y, cv=cv)
-        # plt.subplots(1, 1, figsize=(16, 7))
-        # plt.plot(times, scores.mean(0))
-        # plt.axhline(y=0.25, ls="dashed", color="k")
-        # plt.axvline(x=0, ls="dashed", color="k")
-        # pval = decod_stats(scores - chance)
-        # sig = pval < .05
-        # plt.fill_between(times, chance, scores.mean(0), where=sig, alpha=1)
-        # plt.title(f'{subject}')
-        # plt.savefig(op.join(figures, '%s.png' % subject))
-        # plt.close()
-        # scores_0.append(scores)
-        # clf.fit(X, y)
-        
-        # there is only randoms in practice sessions
-        for train_0, test_0 in cv.split(X_0, y_0):
-            clf.fit(X_0[train_0], y_0[train_0])
-            scores_0.append(np.array(clf.score(X_0[test_0], y_0[test_0])))
-            
-        for i, score_list in zip(range(1, len(all_epo)), [scores_1, scores_2, scores_3, scores_4]):
-            X = all_epo[i].get_data()
-            y = all_beh[i].positions
-            assert len(X) == len(y)
-            # all
-            if trial_type == 'all':
-                for train, test in cv.split(X, y):
-                    score_list.append(np.array(clf.score(X[test], y[test])))
-            elif trial_type == 'pattern':
-                # pattern
-                pattern = all_beh[i].trialtypes == 1
-                X_pat = X[pattern]
-                y_pat = np.array(y[pattern])
-                for train, test in cv.split(X_pat, y_pat):
-                    score_list.append(np.array(clf.score(X_pat[test], y_pat[test])))
-            else:
-                # random
-                random = all_beh[i].trialtypes == 2
-                X_rdm = X[random]
-                y_rdm = np.array(y[random])
-                for train, test in cv.split(X_rdm, y_rdm):
-                    score_list.append(np.array(clf.score(X_rdm[test], y_rdm[test])))
+# scores_0 = list()
+# accuracy = list()
+# for train_0, test_0 in cv.split(X_0, y_0):
+#     clf.fit(X_0[train_0], y_0[train_0])
+#     scores_0.append(np.array(clf.score(X_0[test_0], y_0[test_0])))
+#     y_true = y_0[test_0]
+#     y_pred = clf.predict(X_0[test_0])
+#     for t in range(len(times)):
+#         accuracy.append(np.array(accuracy_score(y_true, y_pred[:, t])))
+# accuracy = np.array(accuracy)
 
-    scores_0 = np.array(scores_0)
-    scores_1 = np.array(scores_1)
-    scores_2 = np.array(scores_2)
-    scores_3 = np.array(scores_3)
-    scores_4 = np.array(scores_4)
-            
-    for score, label in zip([scores_0, scores_1, scores_2, scores_3, scores_4], ['train_prac', 'test_b1', 'test_b2', 'test_b3', 'test_b4']): 
-        plt.subplots(1, 1, figsize=(16, 7))
-        plt.plot(times, score.mean(0).T, label=label)
-        pval = decod_stats(score - chance)
-        sig = pval < .05
-        plt.fill_between(times, chance, score.mean(0).T, where=sig, alpha=.3)
-        plt.legend()
-        plt.title("train on practice")
-        plt.axhline(y=0.25, ls="dashed", color="k", alpha=.2)
-        plt.axvline(x=0, ls="dashed", color="k", alpha=.2)
-        plt.savefig(op.join(figures, trial_type, "%s.png" % label))
-        plt.close()
-        
-    plt.subplots(1, 1, figsize=(16, 7))
-    plt.plot(times, scores_1.mean(0).T, label='block_1')
-    plt.plot(times, scores_2.mean(0).T, label='block_2')
-    plt.plot(times, scores_3.mean(0).T, label='block_3')
-    plt.plot(times, scores_4.mean(0).T, label='block_4')
-    for score in [scores_0, scores_1, scores_2, scores_3, scores_4]:
-        pval = decod_stats(score - chance)
-        sig = pval < .05
-        plt.fill_between(times, chance, score.mean(0).T, where=sig, alpha=.3)
-    plt.legend()
-    plt.title("train on practice_combined")
-    plt.axhline(y=0.25, ls="dashed", color="k", alpha=.2)
-    plt.axvline(x=0, ls="dashed", color="k", alpha=.2)
-    plt.savefig(op.join(figures, trial_type, "combined.png"))
-    plt.close()
+# confuse = list()
+# for t in range(len(times)):
+#     y_pred = clf.predict(X_0[test_0])
+#     confuse.append(ConfusionMatrixDisplay.from_estimator(clf, y_pred, y_0[test_0]))
+
+pred = list()
+# there is only randoms in practice sessions
+for train_0, test_0 in cv.split(X_0, y_0):
+    clf.fit(X_0[train_0], y_0[train_0])
+    pred.append(np.array(clf.predict_proba(X_0[test_0])))
+pred = np.array(pred)
+
+plt.plot(times, pred)
