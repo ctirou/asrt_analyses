dee57533a1b3dce8458b0868f75dc2ff7bbe989c
ctirou <102290014+ctirou@users.noreply.github.com>
2024-06-24 19:22:18 +0200

    seg bash file fix

--- parc_and_seg.sh
+++ parc_and_seg.sh
@@ -7,7 +7,7 @@ source $FREESURFER_HOME/SetUpFreeSurfer.sh
 ls *.nii | parallel jobs 15 recon-all -s {.} -i {} -all -qcache
 
 ### SEGMENTATION -- BRAINNETOME ATLAS --
-cd /Users/coum/Library/CloudStorage/OneDrive-etu.univ-lyon1.fr/asrt/BN_Atlas_freesurfer
+cd /Users/coum/Library/CloudStorage/OneDrive-etu.univ-lyon1.fr/asrt/freesurfer
 export FREESURFER_HOME="/Applications/freesurfer/7.3.2"
 export SUBJECTS_DIR=/Users/coum/Library/CloudStorage/OneDrive-etu.univ-lyon1.fr/asrt/freesurfer
 source $FREESURFER_HOME/SetUpFreeSurfer.sh
@@ -24,6 +24,41 @@ for Subject in "${subjects[@]}"; do
     aparcstats2table -s $Subject --hemi lh --parc BN_Atlas --meas thickness --tablefile ./$Subject/lh.thickness.txt
     mris_anatomical_stats -mgz -cortex $SUBJECTS_DIR/$Subject/label/rh.cortex.label -f $SUBJECTS_DIR/$Subject/stats/rh.BN_Atlas.stats -b -a $SUBJECTS_DIR/$Subject/label/rh.BN_Atlas.annot -c $SUBJECTS_DIR/BN_Atlas_210_LUT.txt $Subject rh white
     aparcstats2table -s $Subject --hemi rh --parc BN_Atlas --meas thickness --tablefile ./$Subject/rh.thickness.txt
+
+    ### convert to CIFTI files
+    mkdir -p $Subject/Native
+    mris_convert --annot ./$Subject/label/lh.BN_Atlas.annot ./$Subject/surf/lh.white ./$Subject/Native/$Subject.L.BN_Atlas.native.label.gii
+    wb_command -set-structure ./$Subject/Native/$Subject.L.BN_Atlas.native.label.gii CORTEX_LEFT
+    mris_convert --annot ./$Subject/label/rh.BN_Atlas.annot ./$Subject/surf/rh.white ./$Subject/Native/$Subject.R.BN_Atlas.native.label.gii
+    wb_command -set-structure ./$Subject/Native/$Subject.R.BN_Atlas.native.label.gii CORTEX_RIGHT
+
+    mris_convert ./$Subject/surf/lh.white ./$Subject/Native/$Subject.L.white.native.surf.gii
+    wb_command -set-structure ./$Subject/Native/$Subject.L.white.native.surf.gii CORTEX_LEFT -surface-type ANATOMICAL -surface-secondary-type GRAY_WHITE
+    mris_convert ./$Subject/surf/rh.white ./$Subject/Native/$Subject.R.white.native.surf.gii
+    wb_command -set-structure ./$Subject/Native/$Subject.R.white.native.surf.gii CORTEX_RIGHT -surface-type ANATOMICAL -surface-secondary-type GRAY_WHITE
+
+    mris_convert ./$Subject/surf/lh.pial ./$Subject/Native/$Subject.L.pial.native.surf.gii
+    wb_command -set-structure ./$Subject/Native/$Subject.L.pial.native.surf.gii CORTEX_LEFT -surface-type ANATOMICAL -surface-secondary-type PIAL
+    mris_convert ./$Subject/surf/rh.pial ./$Subject/Native/$Subject.R.pial.native.surf.gii
+    wb_command -set-structure ./$Subject/Native/$Subject.R.pial.native.surf.gii CORTEX_RIGHT -surface-type ANATOMICAL -surface-secondary-type PIAL
+
+    wb_command -surface-average ./$Subject/Native/$Subject.L.midthickness.native.surf.gii -surf $Subject/Native/$Subject.L.white.native.surf.gii -surf ./$Subject/Native/$Subject.L.pial.native.surf.gii
+    wb_command -set-structure ./$Subject/Native/$Subject.L.midthickness.native.surf.gii CORTEX_LEFT -surface-type ANATOMICAL -surface-secondary-type MIDTHICKNESS
+
+    wb_command -surface-average ./$Subject/Native/$Subject.R.midthickness.native.surf.gii -surf $Subject/Native/$Subject.R.white.native.surf.gii -surf ./$Subject/Native/$Subject.R.pial.native.surf.gii
+    wb_command -set-structure ./$Subject/Native/$Subject.L.midthickness.native.surf.gii CORTEX_RIGHT -surface-type ANATOMICAL -surface-secondary-type MIDTHICKNESS
+
+    ### generate Workbench spec file
+    cd $Subject/Native
+    wb_command -add-to-spec-file ./$Subject.native.wb.spec CORTEX_LEFT ./$Subject.L.white.native.surf.gii
+    wb_command -add-to-spec-file ./$Subject.native.wb.spec CORTEX_LEFT ./$Subject.L.pial.native.surf.gii
+    wb_command -add-to-spec-file ./$Subject.native.wb.spec CORTEX_LEFT ./$Subject.L.midthickness.native.surf.gii
+    wb_command -add-to-spec-file ./$Subject.native.wb.spec CORTEX_LEFT ./$Subject.L.BN_Atlas.native.label.gii
+    wb_command -add-to-spec-file ./$Subject.native.wb.spec CORTEX_RIGHT ./$Subject.R.white.native.surf.gii
+    wb_command -add-to-spec-file ./$Subject.native.wb.spec CORTEX_RIGHT ./$Subject.R.pial.native.surf.gii
+    wb_command -add-to-spec-file ./$Subject.native.wb.spec CORTEX_RIGHT ./$Subject.R.midthickness.native.surf.gii
+    wb_command -add-to-spec-file ./$Subject.native.wb.spec CORTEX_RIGHT ./$Subject.R.BN_Atlas.native.label.gii
+
     ### mapping BN_atlas subcortex to subjects 
     mri_ca_label $SUBJECTS_DIR/$Subject/mri/brain.mgz $SUBJECTS_DIR/$Subject/mri/transforms/talairach.m3z $SUBJECTS_DIR/BN_Atlas_subcortex.gca $SUBJECTS_DIR/$Subject/mri/BN_Atlas_subcortex_aseg.mgz
     ### Segmentation stats
