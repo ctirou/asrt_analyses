3fa8324d9e87377d19000913c8f1eeeea0ebdcd6
ctirou <102290014+ctirou@users.noreply.github.com>
2024-10-11 15:28:09 +0200

    plot_RTs is done 🙌

--- behav_/plot_RTs.py
+++ behav_/plot_RTs.py
@@ -2,12 +2,10 @@ import os.path as op
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
-from config import DATA_DIR, RESULTS_DIR, SUBJS, NEW_FIG_DIR
-
+from config import DATA_DIR, SUBJS, NEW_FIG_DIR
+from tqdm.auto import tqdm
 
 path_data = DATA_DIR
-# path_plots = op.join(RESULTS_DIR, 'figures', 'behav')
 figures_dir = NEW_FIG_DIR
 
 subjects = SUBJS
@@ -26,16 +24,23 @@ random_RT = {
     'Epoch_4': list(),
 }
 
-x = ['1', '2', '3', '4']
+sessions = ['1', '2', '3', '4']
 n = len(subjects)
 
-for subject in subjects:
+subdict = {}
+
+for subject in tqdm(subjects):
 
     all_RT = list()
     pat_RT = list()
     rand_RT = list()
-
+    
+    subdict[subject] = {}
+    
     for i in range(1, 5):
+
+        subdict[subject][i] = {"pattern": [], 
+                               "random": []}
         
         fname_behav = op.join(path_data, 'behav', f'{subject}-{i}.pkl')
         behav_df = pd.read_pickle(fname_behav)
@@ -45,53 +50,37 @@ for subject in subjects:
             all_RT.append(k)
             if behav_df['triplets'][j] == 30:
                 pattern_RT[f'Epoch_{i}'].append(behav_df['RTs'][j]) # faire liste et moyennes puis append contenu liste dans dico
-            elif behav_df['triplets'][j] == 32 or  behav_df['triplets'][j] == 34:
+                subdict[subject][i]["pattern"].append((behav_df['RTs'][j])) 
+            elif behav_df['triplets'][j] == 32 or behav_df['triplets'][j] == 34:
                 random_RT[f'Epoch_{i}'].append(behav_df['RTs'][j])
+                subdict[subject][i]["random"].append((behav_df['RTs'][j]))
+        
+        subdict[subject][i]["pattern"] = np.mean(subdict[subject][i]["pattern"]) 
+        subdict[subject][i]["random"] = np.mean(subdict[subject][i]["random"])
 
-mpat = list()
-spat = list()
-mrand = list()
-srand = list()
-
-for i in range(1, 5):
-    
-    spat.append(np.std(pattern_RT[f'Epoch_{i}']))
-    mpat.append(np.mean(pattern_RT[f'Epoch_{i}']))
-    
-    srand.append(np.std(random_RT[f'Epoch_{i}']))
-    mrand.append(np.mean(random_RT[f'Epoch_{i}']))
-
-pdf = pd.DataFrame.from_dict(pattern_RT, orient='index').T
-pdf.rename(columns={'Epoch_1':'1','Epoch_2':'2','Epoch_3':'3','Epoch_4':'4'}, inplace=True)
-
-rdf = pd.DataFrame.from_dict(random_RT, orient='index').T
-rdf.rename(columns={'Epoch_1':'1','Epoch_2':'2','Epoch_3':'3','Epoch_4':'4'}, inplace=True)
-
-learn_index = (rdf - pdf)/rdf
-learn_index.dropna()
-
-x = pdf.columns
-
-perr = np.std(pdf)/np.sqrt(n)
-rerr = np.std(rdf)/np.sqrt(n)
-
-t = np.mean(learn_index)
-sdt = np.std(learn_index)/np.sqrt(n)
+# Calculate means and standard errors
+mean_pattern = [np.mean(pattern_RT[f'Epoch_{i}']) for i in range(1, 5)]
+mean_random = [np.mean(random_RT[f'Epoch_{i}']) for i in range(1, 5)]
+stderr_pattern = [np.std(pattern_RT[f'Epoch_{i}']) / np.sqrt(n) for i in range(1, 5)]
+stderr_random = [np.std(random_RT[f'Epoch_{i}']) / np.sqrt(n) for i in range(1, 5)]
 
 color1 = "#70AD47"
-color2 = "#5B9BD5"
-fig, ax = plt.subplots(1, 1, figsize=(8, 5))
+color2 = "C7"
+fig, ax = plt.subplots(1, 1, figsize=(7, 5))
 ax.autoscale()
-ax.plot(x, np.mean(pdf), label="Paired elements", color=color1)
-ax.plot(x, np.mean(rdf), label="Unpaired elements", color=color2)
-ax.errorbar(x, np.mean(pdf), yerr=perr, fmt='-o', color=color1, alpha=.7)
-ax.errorbar(x, np.mean(rdf), yerr=rerr, fmt='-o', color=color2, alpha=.7)
-ax.legend(loc='lower left', frameon=False, title='n = 11')
+ax.plot(sessions, mean_pattern, '-o', color=color1, label="Paired elements", markersize=7, alpha=1)
+ax.plot(sessions, mean_random, '-o', color=color2, label="Unpaired elements", markersize=7, alpha=.9)
+# Scatter plot individual subject values
+for subject in subjects:
+    for i in range(1, 5):
+        ax.scatter(str(i), subdict[subject][i]["pattern"], color=color1, alpha=0.3)
+        ax.scatter(str(i), subdict[subject][i]["random"], color=color2, alpha=0.2)
+# Add asterisks above all mean random values
+for i, mean_r in enumerate(mean_random):
+    ax.annotate('*', (sessions[i], mean_r + 15), ha='center', color='black', fontsize=14)
+ax.legend(loc='lower left', frameon=False)
 ax.set_xlabel("Session")
 ax.set_ylabel("Reaction Time (ms)")
 ax.spines['top'].set_visible(False)
 ax.spines['right'].set_visible(False)
-# Add asterisks above all error bars
-for i, (mean_rdf, err_rdf) in enumerate(zip(np.mean(rdf), rerr)):
-    ax.annotate('*', (x[i], mean_rdf + err_rdf + 0.2), ha='center', color='black', fontsize=14)
 fig.savefig(figures_dir / 'behav' / 'mean_RT2.pdf')
