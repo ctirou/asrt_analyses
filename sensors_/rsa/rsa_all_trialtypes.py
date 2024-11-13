28e325f43bef1b0d0794f3a35dcb9be118dfbb35
ctirou <102290014+ctirou@users.noreply.github.com>
2024-11-13 17:32:22 +0100

    rsa_all_trialtypes is ok 🍕

--- sensors_/rsa/rsa_all_trialtypes.py
+++ sensors_/rsa/rsa_all_trialtypes.py
@@ -4,13 +4,12 @@ import numpy as np
 import mne
 import pandas as pd
 import matplotlib.pyplot as plt
-from scipy.stats import ttest_1samp, zscore, spearmanr
+from scipy.stats import ttest_1samp, spearmanr
 from tqdm.auto import tqdm
 from base import *
 from config import *
 
 lock = 'stim'
-trial_type = 'pattern'
 analysis = 'usual'
 overwrite = False
 
@@ -57,7 +56,7 @@ for subject in tqdm(subjects):
     # loop across sessions
     for epoch_num in [0, 1, 2, 3, 4]:
         
-        if not op.exists(res_path / f"pat-{epoch_num}.npy") or not op.exists(res_path / f"rand-{epoch_num}.npy"):
+        if not op.exists(res_path / f"pat-{epoch_num}.npy") or not op.exists(res_path / f"rand-{epoch_num}.npy") or overwrite:
             # read behav
             behav_fname = op.join(data_path, "behav/%s-%s.pkl" % (subject, epoch_num))
             behav = pd.read_pickle(behav_fname)
@@ -71,8 +70,8 @@ for subject in tqdm(subjects):
             rdm_pat = get_rdm(epoch_pat, behav_pat)
             np.save(res_path / f"pat-{epoch_num}.npy", rdm_pat)
         
-            epoch_rand = epoch[np.where(behav["triplets"]==34)].get_data(copy=False).mean(axis=0)
-            behav_rand = behav[behav["triplets"]==34]
+            epoch_rand = epoch[np.where(behav["trialtypes"]==2)].get_data(copy=False).mean(axis=0)
+            behav_rand = behav[behav["trialtypes"]==2]
             assert len(epoch_rand) == len(behav_rand)
             rdm_rand = get_rdm(epoch_rand, behav_rand)
             np.save(res_path / f"rand-{epoch_num}.npy", rdm_rand)
@@ -168,50 +167,107 @@ for subject in tqdm(subjects):
     for pair, rev_pair, pat_sim, rand_sim in zip(pairs, rev_pairs, similarities, random_lows):
         if ((pair in pairs_in_sequence) or (rev_pair in pairs_in_sequence)):
             in_seq.append(pat_sim)
+            # pat high rdm high
+            # out_seq.append(rand_sim)
         else:
+            # usual: paired - unapaired
             out_seq.append(pat_sim)
-    # for l in random_lows:
-    #     out_seq.append(l)
-    
+            # pat high rdm low
+            # out_seq.append(rand_sim)
     all_in_seqs.append(np.array(in_seq))    
     all_out_seqs.append(np.array(out_seq))
     
 all_in_seqs = np.array(all_in_seqs)
 all_out_seqs = np.array(all_out_seqs)
 
-diff_inout = all_in_seqs.mean(axis=1) - all_out_seqs.mean(axis=1)
+diff_prac = all_out_seqs[:, :, 0, :].mean(axis=1) - all_in_seqs[:, :, 0, :].mean(axis=1)
+diff_learn = all_out_seqs[:, :, 1:, :].mean(axis=(1)) - all_in_seqs[:, :, 1:, :].mean(axis=(1))
 
+# # for sub in range(len(subjects)):
+# for sub in range(1):
+#     plt.subplots(1, 1, figsize=(14, 5))
+#     plt.plot(times, diff_prac[sub, :], label='practice')
+#     for i in range(4):
+#         plt.plot(times, diff_learn[sub, i, :], label='epoch %s' % (i+1))
+#     plt.title(f"{sub}")
+#     plt.legend()
+#     plt.show()
+
+color1 = "#008080"
+color2 = "#FFA500"
 # plot in out and diff
 plt.subplots(1, 1, figsize=(14, 5))
-plt.plot(times, all_in_seqs.mean((0, 1, 2)), label='in', color='C0', alpha=0.6)
-plt.plot(times, all_out_seqs.mean((0, 1, 2)), label='out', color='C1', alpha=0.6)
-plt.plot(times, diff_inout.mean((0, 1)), label='in - out', color='C2', alpha=0.6)
+plt.plot(times, all_in_seqs[:, :, 1:, :].mean((0, 1, 2)), label='high', color='C0', alpha=0.6)
+plt.plot(times, all_out_seqs[:, :, 1:, :].mean((0, 1, 2)), label='low', color='C1', alpha=0.6)
 plt.axvspan(0, 0.2, color='grey', alpha=.2)
 plt.legend()
-plt.savefig(op.join(figures_dir, '%s_all_%s.pdf' % (metric, trial_type)), transparent=True)
+plt.title(f'{metric} high vs. low {analysis}', style='italic')
+plt.savefig(op.join(figures_dir, 'low_high.pdf'), transparent=True)
 plt.close()
 
+# plot the difference in vs. out sequence averaging all epochs
+plt.subplots(1, 1, figsize=(14, 5))
+plt.plot(times, diff_prac.mean(0), label='practice', color='C7', alpha=0.6)
+plt.plot(times, diff_learn.mean((0, 1)), label='low - high', color='C0', alpha=0.6)
+plt.plot(times, diff_learn.mean((0, 1))  - diff_prac.mean(0), label='low - high - practice', color='C1', alpha=0.6)
+p_values_unc = ttest_1samp(diff_learn.mean(1), axis=0, popmean=0)[1]
+sig_unc = p_values_unc < 0.05
+p_values = decod_stats(diff_learn.mean(1), -1)
+sig = p_values < 0.05
+plt.fill_between(times, 0, diff_learn.mean((0, 1))  - diff_prac.mean(0), where=sig_unc, color=color1, alpha=0.2, label='uncorrected')
+plt.fill_between(times, 0, diff_learn.mean((0, 1))  - diff_prac.mean(0), where=sig, color=color2, alpha=0.3, label='corrected')
+plt.axhline(0, color='black', linestyle='dashed')
+plt.axvspan(0, 0.2, color='grey', alpha=.2)
+plt.legend()
+plt.title(f'{metric} low - high average {analysis}', style='italic')
+plt.savefig(op.join(figures_dir, 'low_high_ave.pdf'))
+plt.close()
+
+# plot the difference in vs. out sequence for each epoch
+for i in range(4):
+    plt.subplots(1, 1, figsize=(14, 5))
+    plt.plot(times, diff_prac.mean(0), label='practice', color='C7', alpha=0.6)
+    plt.plot(times, diff_learn[:, i, :].mean(0) - diff_prac.mean(0), label='low - high - practice', color='C1', alpha=0.6)
+    diff = diff_learn[:, i, :] - diff_prac
+    p_values_unc = ttest_1samp(diff, axis=0, popmean=0)[1]
+    sig_unc = p_values_unc < 0.05
+    p_values = decod_stats(diff, -1)
+    sig = p_values < 0.05
+    plt.fill_between(times, 0, diff_learn[:, i, :].mean(0) - diff_prac.mean(0), where=sig_unc, alpha=0.2, color=color1, label='uncorrected')
+    plt.fill_between(times, 0, diff_learn[:, i, :].mean(0) - diff_prac.mean(0), where=sig, alpha=0.4, color=color2, label='corrected')
+    plt.axhline(0, color='black', linestyle='dashed')
+    plt.axvspan(0, 0.2, color='grey', alpha=.2)
+    plt.legend()
+    # plt.gca().set_ylim(-0.04, 0.12)
+    plt.title(f'{metric} low - high session {i+1} {analysis}', style='italic')
+    plt.savefig(op.join(figures_dir, 'low_high_%s.pdf' % (str(i+1))))
+    plt.close()
+    
 # plot correlations
-rhos = [[spearmanr([0, 1, 2, 3, 4], diff_inout[sub, :, itime])[0] for itime in range(len(times))] for sub in range(len(subjects))]
+diff = all_out_seqs.mean(axis=1) - all_in_seqs.mean(axis=1)
+rhos = [[spearmanr([0, 1, 2, 3, 4], diff[sub, :, itime])[0] for itime in range(len(times))] for sub in range(len(subjects))]
 rhos = np.array(rhos)
 plt.subplots(1, 1, figsize=(14, 5))
-plt.plot(times, rhos.mean(0))
+plt.plot(times, rhos.mean(0), label='rhos')
+p_values_unc = ttest_1samp(rhos, axis=0, popmean=0)[1]
+sig_unc = p_values_unc < 0.05
 p_values = decod_stats(rhos, -1)
 sig = p_values < 0.05
-plt.fill_between(times, rhos.mean(0), 0, where=sig, color='green', alpha=.7)
+plt.fill_between(times, rhos.mean(0), 0, where=sig_unc, color=color1, alpha=.2, label='uncorrected')
+plt.fill_between(times, rhos.mean(0), 0, where=sig, color=color2, alpha=.4, label='corrected')
 plt.axhline(0, color="black", linestyle="dashed")
-plt.title(f'{metric} correlations', style='italic')
+plt.title(f'{metric} correlations {analysis}', style='italic')
 plt.axvspan(0, 0.2, color='grey', alpha=.2)
 plt.axhline(0, color='black', linestyle='dashed')
-plt.savefig(op.join(figures_dir, '%s_high_low_correlations_%s.pdf' % (metric, trial_type)), transparent=True)
+plt.legend()
+plt.savefig(op.join(figures_dir, 'low_high_correlations.pdf'), transparent=True)
 plt.close()
 
 # learning_index_df = pd.read_csv(FIGURES_DIR / 'behav' / 'learning_indices.csv', sep="\t")
-
 # # plot across subjects
 # all_pvalues = []
 # for t in range(len(times)):
-#     all_pvalues.append(spearmanr(learning_index_df["4"], diff_inout[:, -1, t])[1])
+#     all_pvalues.append(spearmanr(learning_index_df["4"], diff_learn[:, -1, t])[1])
 # plt.subplots(1, 1, figsize=(14, 5))
 # plt.plot(times, all_pvalues)
 # sig = (np.asarray(all_pvalues) < 0.05)
@@ -226,7 +282,7 @@ plt.close()
 # for sub in tqdm(range(len(subjects))):
 #     rhos = []
 #     for t in range(len(times)):
-#         rhos.append(spearmanr(learning_index_df.iloc[sub, 1:], diff_inout[sub, :, t])[0])
+#         rhos.append(spearmanr(learning_index_df.iloc[sub, 1:], diff_learn[sub, :, t])[0])
 #     all_rhos.append(rhos)
 # all_rhos = np.array(all_rhos)
 # plt.subplots(1, 1, figsize=(14, 5))
@@ -240,42 +296,3 @@ plt.close()
 # plt.axhline(0, color='black', linestyle='dashed')
 # plt.savefig(op.join(figures_dir, '%s_high_low_within_sub_correlations_%s.pdf' % (metric, trial_type)), transparent=True)
 # plt.close()
-
-# plot the difference in vs. out sequence averaging all epochs
-plt.subplots(1, 1, figsize=(14, 5))
-diff = diff_inout[:, 1:, :].mean(1) - diff_inout[:, 0, :]
-plt.plot(times, diff_inout[:, 0, :].mean(0), label='practice', color='C7', alpha=0.6)
-plt.plot(times, diff_inout[:, 1:, :].mean(1).mean(0), label='high - low', color='C0', alpha=0.6)
-plt.plot(times, diff.mean(0), label='high - low - practice', color='C1', alpha=0.6)
-p_values_unc = ttest_1samp(diff, axis=0, popmean=0)[1]
-sig_unc = p_values_unc < 0.05
-p_values = decod_stats(diff, -1)
-sig = p_values < 0.05
-# plt.fill_between(times, 0, diff_inout.mean((0, 1)), where=sig_unc, color='C1', alpha=0.2)
-# plt.fill_between(times, 0, diff.mean(0), where=sig, color='black', alpha=0.3)
-plt.axhline(0, color='black', linestyle='dashed')
-plt.axvspan(0, 0.2, color='grey', alpha=.2)
-plt.legend()
-plt.title(f'{metric} high – low average', style='italic')
-plt.savefig(op.join(figures_dir, '%s_high_low_ave_%s.pdf' % (metric, trial_type)))
-plt.close()
-
-# plot the difference in vs. out sequence for each epoch
-for i in range(1, 5):
-    plt.subplots(1, 1, figsize=(14, 5))
-    # plt.plot(times, diff_inout[:, 0, :].mean(0), label='practice', color='C7', alpha=0.6)
-    plt.plot(times, diff_inout[:, i, :].mean(0), label='learning', color='C1', alpha=0.6)
-    diff = diff_inout[:, i, :] - diff_inout[:, 0, :]
-    p_values_unc = ttest_1samp(diff, axis=0, popmean=0)[1]
-    sig_unc = p_values < 0.05
-    p_values = decod_stats(diff, -1)
-    sig = p_values < 0.05
-    # plt.fill_between(times, 0, diff_inout[:, i, :].mean(0), where=sig_unc, alpha=0.2)
-    # plt.fill_between(times, 0, diff_inout[:, i, :].mean(0), where=sig, alpha=0.4, color='black')
-    # plt.axhline(0, color='black', linestyle='dashed')
-    plt.axvspan(0, 0.2, color='grey', alpha=.2)
-    plt.legend()
-    # plt.gca().set_ylim(-0.04, 0.12)
-    plt.title(f'{metric} high – low epoch {i}', style='italic')
-    plt.savefig(op.join(figures_dir, '%s_high_low_%s_%s.pdf' % (metric, trial_type, str(i))))
-    plt.close()
