60bb947be4ed9cb64d9c42c81d219bfdda083956
ctirou <102290014+ctirou@users.noreply.github.com>
2024-12-03 18:32:42 +0100

    arranged rsa_plot with reverse, as and ws to do 🍕

--- sensors_/rsa/rsa_plot.py
+++ sensors_/rsa/rsa_plot.py
@@ -9,7 +9,7 @@ from tqdm.auto import tqdm
 from base import *
 from config import *
 
-lock = 'button'
+lock = 'stim'
 analysis = 'usual'
 analysis = 'pat_high_rdm_high'
 # analysis = 'pat_high_rdm_low'
@@ -188,21 +188,22 @@ for analysis in analyses:
     # plot reverse difference high vs. low sequence averaging all sessions
     rev_high = all_highs[:, :, 1:, :].mean((1, 2)) - all_highs[:, :, 0, :].mean(axis=1)
     rev_low = all_lows[:, :, 1:, :].mean((1, 2)) - all_lows[:, :, 0, :].mean(axis=1)
-    diff = rev_low - rev_high
+    rev_diff = rev_low - rev_high
+    
     plt.subplots(1, 1, figsize=(16, 11))
-    plt.plot(times,  diff.mean(0), label='(low_post - low_pre) - (high_post - high_pre)', color='C1', alpha=0.6)
+    plt.plot(times, rev_diff.mean(0), label='(low_post - low_pre) - (high_post - high_pre)', color='C1', alpha=0.6)
     plt.plot(times, rev_high.mean(0), label='high_post - high_pre', alpha=0.6)
     plt.plot(times, rev_low.mean(0), label='low_post - low_pre', alpha=0.6)
     plt.plot(times, all_highs[:, :, 1:, :].mean((0, 1, 2)), label='high_post')
     plt.plot(times, all_highs[:, :, 0, :].mean((0, 1)), label='high_pre')
     plt.plot(times, all_lows[:, :, 1:, :].mean((0, 1, 2)), label='low_post')
     plt.plot(times, all_lows[:, :, 0, :].mean((0, 1)), label='low_pre')
-    p_values_unc = ttest_1samp(diff,  axis=0, popmean=0)[1]
+    p_values_unc = ttest_1samp(rev_diff,  axis=0, popmean=0)[1]
     sig_unc = p_values_unc < 0.05
-    p_values = decod_stats(diff, -1)
+    p_values = decod_stats(rev_diff, -1)
     sig = p_values < 0.05
-    plt.fill_between(times, 0, diff.mean(0), where=sig_unc, color=color1, alpha=0.2, label='uncorrected')
-    plt.fill_between(times, 0, diff.mean(0), where=sig, color=color2, alpha=0.3, label='corrected')
+    plt.fill_between(times, 0, rev_diff.mean(0), where=sig_unc, color=color1, alpha=0.2, label='uncorrected')
+    plt.fill_between(times, 0, rev_diff.mean(0), where=sig, color=color2, alpha=0.3, label='corrected')
     plt.axhline(0, color='black', linestyle='dashed')
     if lock == 'stim':
         plt.axvspan(0, 0.2, color='grey', alpha=.2)
@@ -217,21 +218,21 @@ for analysis in analyses:
     for i in range(1, 5): 
         rev_high = all_highs[:, :, i, :].mean(1) - all_highs[:, :, 0, :].mean(axis=1)
         rev_low = all_lows[:, :, i, :].mean(1) - all_lows[:, :, 0, :].mean(axis=1)
-        diff = rev_low - rev_high
+        diff_corr = rev_low - rev_high
         plt.subplots(1, 1, figsize=(16, 11))
-        plt.plot(times,  diff.mean(0), label='(low_post - low_pre) - (high_post - high_pre)', color='C1', alpha=0.6)
+        plt.plot(times,  diff_corr.mean(0), label='(low_post - low_pre) - (high_post - high_pre)', color='C1', alpha=0.6)
         plt.plot(times, rev_high.mean(0), label='high_post - high_pre', alpha=0.6)
         plt.plot(times, rev_low.mean(0), label='low_post - low_pre', alpha=0.6)
         plt.plot(times, all_highs[:, :, i, :].mean((0, 1)), label='high_post')
         plt.plot(times, all_highs[:, :, 0, :].mean((0, 1)), label='high_pre')
         plt.plot(times, all_lows[:, :, i, :].mean((0, 1)), label='low_post')
         plt.plot(times, all_lows[:, :, 0, :].mean((0, 1)), label='low_pre')
-        p_values_unc = ttest_1samp(diff,  axis=0, popmean=0)[1]
+        p_values_unc = ttest_1samp(diff_corr,  axis=0, popmean=0)[1]
         sig_unc = p_values_unc < 0.05
-        p_values = decod_stats(diff, -1)
+        p_values = decod_stats(diff_corr, -1)
         sig = p_values < 0.05
-        plt.fill_between(times, 0, diff.mean(0), where=sig_unc, color=color1, alpha=0.2, label='uncorrected')
-        plt.fill_between(times, 0, diff.mean(0), where=sig, color=color2, alpha=0.3, label='corrected')
+        plt.fill_between(times, 0, diff_corr.mean(0), where=sig_unc, color=color1, alpha=0.2, label='uncorrected')
+        plt.fill_between(times, 0, diff_corr.mean(0), where=sig, color=color2, alpha=0.3, label='corrected')
         plt.axhline(0, color='black', linestyle='dashed')
         if lock == 'stim':
             plt.axvspan(0, 0.2, color='grey', alpha=.2)
@@ -242,50 +243,80 @@ for analysis in analyses:
         plt.savefig(op.join(figures_dir, f'rev_low_high_{i}.pdf'))
         plt.close()
 
-    # reverse difference high vs. low sequence across sessions
-    diff = list()
-    for i in range(1, 5): 
+    # plot reverse correlations
+    rev_diff_corr = list()   
+    for i in range(5):
         rev_high = all_highs[:, :, i, :].mean(1) - all_highs[:, :, 0, :].mean(axis=1)
         rev_low = all_lows[:, :, i, :].mean(1) - all_lows[:, :, 0, :].mean(axis=1)
-        diff.append(rev_low - rev_high)
-    diff = np.array(diff)
-    rhos = [[spear([0, 1, 2, 3], diff[:, sub, itime])[0] for itime in range(len(times))] for sub in range(len(subjects))]
+        rev_diff_corr.append(rev_low - rev_high)
+    rev_diff_corr = np.array(rev_diff_corr)
+    rhos = [[spear([0, 1, 2, 3, 4], rev_diff_corr[:, sub, itime])[0] for itime in range(len(times))] for sub in range(len(subjects))]
     rhos = np.array(rhos)
-    plt.subplots(1, 1)
+    plt.subplots(1, 1, figsize=(16, 11))
     plt.plot(times, rhos.mean(0))
-    pval = decod_stats(rhos, -1)
-    sig = pval < .05
-    plt.fill_between(times, 0, rhos.mean(0), where=sig, color=color2, alpha=.4)
+    p_values_unc = ttest_1samp(rhos, axis=0, popmean=0)[1]
+    sig_unc = p_values_unc < 0.05
+    p_values = decod_stats(rhos, -1)
+    sig = p_values < .05
+    plt.fill_between(times, 0, rhos.mean(0), where=sig, color=color2, alpha=.3, label='corrected')
+    plt.fill_between(times, 0, rhos.mean(0), where=sig_unc, color=color1, alpha=.2, label='uncorrected')
     plt.axhline(0, color='black', linestyle='dashed')
     if lock == 'stim':
         plt.axvspan(0, 0.2, color='grey', alpha=.2)
     else:
         plt.axvline(0, color='black')
+    plt.legend()
     plt.title(f'reverse low - high {analysis} correlations')
     plt.savefig(op.join(figures_dir, 'rev_low_high_corr.pdf'))
     plt.close()
-    
-    
-# # Get time points average
-# idx = np.where((times >= 0.03) & (times <= 0.5))[0]
-# diff = diff_learn - diff_prac
-# diff = np.mean(diff[:, idx], axis=1)
 
-# mean_diff = np.mean(diff)
+    ### TODO ###
 
-# mean_learn_idx = np.mean(learn_index_df, axis=0)
+    learn_index_df = pd.read_csv(FIGURES_DIR / 'behav' / 'learning_indices.csv', sep="\t", index_col=0)
+    # plot across subjects
+    all_pvalues, all_rhos = [], []
+    for t in range(len(times)):
+        rho, pval = spear(learn_index_df["4"], rev_diff[:, -1, t])
+        all_rhos.append(rho)
+        all_pvalues.append(pval)
+    plt.subplots(1, 1, figsize=(14, 5))
+    plt.plot(times, all_rhos, label='rho')
+    sig = (np.asarray(all_pvalues) < 0.05)
+    plt.fill_between(times, all_rhos, 0, where=sig, color=color2, alpha=.4, label='significant')  # Solid line at the bottom when sig is true
+    plt.title(f'{metric} across subjects corr {analysis}', style='italic')
+    if lock == 'stim':
+        plt.axvspan(0, 0.2, color='grey', alpha=.2)
+    else:
+        plt.axvline(0, color='black')
+    plt.axhline(0, color='black', linestyle='dashed')
+    plt.legend()
+    plt.savefig(op.join(figures_dir, 'low_high_across_sub_corr.pdf'), transparent=True)
+    plt.close()
 
-# # Compute Spearman correlation with updated `diff`
-# all_rhos = []
-# for sub in tqdm(range(len(subjects))):
-#     # Correlation between learning index and mean diff for the subject
-#     rhos = spear(learn_index_df.iloc[sub, :], diff[sub, :])[0]
-#     all_rhos.append(rhos)
-
-# all_rhos = np.array(all_rhos)
-
-# # Optionally calculate the mean rho across subjects
-# mean_rho = np.mean(all_rhos)
-
-# # Print or visualize results
-# print(f"Mean Spearman correlation across subjects: {mean_rho}")
+    # plot within subjects
+    diff = all_lows.mean(1) - all_highs.mean(1)
+    all_rhos = []
+    for sub in tqdm(range(len(subjects))):
+        rhos = []
+        for t in range(len(times)):
+            rhos.append(spear(learn_index_df.iloc[sub, :], diff[sub, :, t])[0])
+        all_rhos.append(rhos)
+    all_rhos = np.array(all_rhos)
+    plt.subplots(1, 1, figsize=(14, 5))
+    plt.plot(times, all_rhos.mean(0), label='rhos')
+    p_values_unc = ttest_1samp(all_rhos, axis=0, popmean=0)[1]
+    sig_unc = p_values_unc < 0.05
+    p_values = decod_stats(all_rhos, -1)
+    sig = p_values < 0.05
+    plt.fill_between(times, all_rhos.mean(0), 0, where=sig_unc, color=color1, alpha=.2, label='uncorrected')
+    plt.fill_between(times, all_rhos.mean(0), 0, where=sig, color=color2, alpha=.4, label='corrected')
+    plt.axhline(0, color="black", linestyle="dashed")
+    plt.title(f'{metric} within subjects corr {analysis}', style='italic')
+    if lock == 'stim':
+        plt.axvspan(0, 0.2, color='grey', alpha=.2)
+    else:
+        plt.axvline(0, color='black')
+    plt.axhline(0, color='black', linestyle='dashed')
+    plt.legend()
+    plt.savefig(op.join(figures_dir, 'low_high_within_sub_corr.pdf'), transparent=True)
+    plt.close()
