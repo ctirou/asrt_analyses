d131478bbd2aa08dc3082cc4550d303df5e05e02
ctirou <102290014+ctirou@users.noreply.github.com>
2024-12-17 13:11:33 +0100

    better plot of rsa figure, and decoding in sensor space 🍕

---
+++ sensors_/decoding.py
@@ -0,0 +1,87 @@
+import os.path as op
+import numpy as np
+import pandas as pd
+import mne
+from mne.decoding import SlidingEstimator, cross_val_multiscore
+from sklearn.pipeline import make_pipeline
+from sklearn.model_selection import StratifiedKFold
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from base import ensure_dir, get_volume_estimate_time_course
+from config import *
+import gc
+import os
+from tqdm.auto import tqdm
+
+# params
+subjects = SUBJS
+
+analysis = "decoding"
+lock = "button" # "stim", "button"
+trial_type = 'pattern' # "all", "pattern", or "random"
+data_path = DATA_DIR
+subjects_dir = FREESURFER_DIR
+sessions = ['Practice', 'Block_1', 'Block_2', 'Block_3', 'Block_4']
+folds = 10
+solver = 'lbfgs'
+scoring = "accuracy"
+verbose = True
+jobs = -1
+
+overwrite = False
+
+res_path = RESULTS_DIR / 'decoding' / 'sensors' / lock / trial_type
+ensure_dir(res_path)
+
+# set-up the classifier and cv structure
+clf = make_pipeline(StandardScaler(), LogisticRegression(C=1.0, max_iter=100000, solver=solver, class_weight="balanced", random_state=42))
+clf = SlidingEstimator(clf, scoring=scoring, n_jobs=jobs, verbose=verbose)
+cv = StratifiedKFold(folds, shuffle=True)
+
+for subject in subjects:
+    
+    print(f"Processing {subject}")
+            
+    epo_dir = data_path / lock
+    epo_fnames = [epo_dir / f'{f}' for f in sorted(os.listdir(epo_dir)) if '.fif' in f and subject in f]
+    all_epo = [mne.read_epochs(fname, preload=True, verbose="error") for fname in epo_fnames]
+    for epoch in all_epo: # see mne.preprocessing.maxwell_filter to realign the runs to a common head position. On raw data.
+        epoch.info['dev_head_t'] = all_epo[0].info['dev_head_t']
+    epoch = mne.concatenate_epochs(all_epo)
+
+    beh_dir = data_path / 'behav'
+    beh_fnames = [beh_dir / f'{f}' for f in sorted(os.listdir(beh_dir)) if '.pkl' in f and subject in f]
+    all_beh = [pd.read_pickle(fname) for fname in beh_fnames]
+    behav = pd.concat(all_beh)
+
+    if lock == 'button': 
+        bsl_data = data_path / "bsl"
+        epoch_bsl_fnames = [bsl_data / f"{f}" for f in sorted(os.listdir(bsl_data)) if ".fif" in f and subject in f]
+        all_bsl = [mne.read_epochs(fname, preload=True, verbose="error") for fname in epoch_bsl_fnames]
+        for epo in all_bsl:
+            epo.info['dev_head_t'] = all_epo[0].info['dev_head_t']
+        epoch_bsl = mne.concatenate_epochs(all_bsl)
+        
+    if not op.exists(res_path / f"{subject}-all-scores.npy") or overwrite:
+        if trial_type == 'pattern':
+            pattern = behav.trialtypes == 1
+            X = epoch.get_data()[pattern]
+            y = behav.positions[pattern]
+        elif trial_type == 'random':
+            random = behav.trialtypes == 2
+            X = epoch.get_data()[random]
+            y = behav.positions[random]
+        else:
+            X = epoch.get_data()
+            y = behav.positions
+        y = y.reset_index(drop=True)            
+        assert X.shape[0] == y.shape[0]
+
+        del epoch
+        gc.collect()
+
+        scores = cross_val_multiscore(clf, X, y, cv=cv, verbose=verbose)
+        np.save(res_path / f"{subject}-all-scores.npy", scores.mean(0))
+            
+        del X, y, scores
+        gc.collect()
--- sensors_/rsa/rsa_figure.py
+++ sensors_/rsa/rsa_figure.py
@@ -26,6 +26,7 @@ figures_dir = FIGURES_DIR / "RSA" / "sensors"
 ensure_dir(figures_dir)
 
 all_highs, all_lows = [], []
+decoding = []
 
 for subject in tqdm(subjects):
     
@@ -35,14 +36,16 @@ for subject in tqdm(subjects):
     # Read the behav file to get the sequence 
     behav_dir = op.join(RAW_DATA_DIR, "%s/behav_data/" % (subject)) 
     sequence = get_sequence(behav_dir)
-
-    high, low = get_all_high_low(res_path, sequence, analysis, cv=True)
-        
+    high, low = get_all_high_low(res_path, sequence, analysis, cv=True)    
     all_highs.append(high)    
     all_lows.append(low)
     
+    res_path = RESULTS_DIR / 'decoding' / 'sensors' / lock / 'pattern'
+    decoding.append(np.load(res_path / f"{subject}-all-scores.npy"))
+    
 all_highs = np.array(all_highs)
 all_lows = np.array(all_lows)
+decoding = np.array(decoding)
 
 high = all_highs[:, :, 1:, :].mean((1, 2)) - all_highs[:, :, 0, :].mean(axis=1)
 low = all_lows[:, :, 1:, :].mean((1, 2)) - all_lows[:, :, 0, :].mean(axis=1)
@@ -57,10 +60,14 @@ diff_sess = np.array(diff_sess).swapaxes(0, 1)
 
 learn_index_df = pd.read_csv(FIGURES_DIR / 'behav' / 'learning_indices.csv', sep="\t", index_col=0)
 
-def sig_span(ax, sig, times):
-    for start, end in zip(times[:-1], times[1:]):
+def sig_span(ax, sig, times, lab=True):
+    for i, (start, end) in enumerate(zip(times[:-1], times[1:])):
         if sig[np.where(times == start)[0][0]]:
-            ax.axvspan(start, end, facecolor="#F2AD00", alpha=0.3, edgecolor=None)
+            if lab:
+                label = 'Significant' if not any(sig[:i]) else None
+            else:
+                label = None
+            ax.axvspan(start, end, facecolor="#F2AD00", alpha=0.3, label=label, edgecolor=None)
 
 innerB = [['B1'],
          ['B2']]
@@ -76,35 +83,56 @@ for ax in axd.values():
     ax.spines['top'].set_visible(False)
     ax.spines['right'].set_visible(False)
     ax.set_prop_cycle(cycler('color', colors['Darjeeling1']))
-    if ax != axd['C']:
+    if ax not in [axd['A'], axd['C']]:
         if lock == 'stim':
-            ax.axvspan(0, 0.2, facecolor='grey', edgecolor=None, alpha=.2)
+            ax.axvspan(0, 0.2, facecolor='grey', edgecolor=None, alpha=.1)
         else:
             ax.axvline(0, color='black')
+
+### A ###            
+if lock == 'stim':
+    axd['A'].axvspan(0, 0.2, facecolor='grey', edgecolor=None, alpha=.1, label='Stimulus onset')
+else:
+    axd['A'].axvline(0, color='black', label='Button press')
+p_values = decod_stats(decoding - 0.25, -1)
+sig = p_values < 0.05
+# sig_span(axd['A'], sig, times)
+sem = np.std(decoding, axis=0) / np.sqrt(len(subjects))
+axd['A'].plot(times, decoding.mean(0), alpha=1)
+axd['A'].fill_between(times, decoding.mean(0) - sem, decoding.mean(0) + sem, alpha=0.2)
+axd['A'].axhline(0.25, color='black', linestyle='dashed')
+axd['A'].set_ylabel('Accuracy')
+axd['A'].fill_between(times, .23, .233, where=sig, alpha=0.3, color="#F2AD00", capstyle='round', label='Significant')
+axd['A'].legend(frameon=False)
+axd['A'].set_title(f'Decoding of Pattern trials', style='italic')
+            
 ### B1 ####        
+axd['B1'].plot(times, diff.mean(0), alpha=1, label='Random - Pattern', zorder=10)
 p_values = decod_stats(diff, -1)
 sig = p_values < 0.05
 sig_span(axd['B1'], sig, times)
 sem = np.std(diff, axis=0) / np.sqrt(len(subjects))
-axd['B1'].plot(times, diff.mean(0), alpha=1, label='Random - Pattern')
-axd['B1'].fill_between(times, diff.mean(0) - sem, diff.mean(0) + sem, alpha=0.2)
+axd['B1'].fill_between(times, diff.mean(0) - sem, diff.mean(0) + sem, alpha=0.2, zorder=5)
 axd['B1'].axhline(0, color='black', linestyle='dashed')
-axd['B1'].legend()
+axd['B1'].legend(frameon=False)
 axd['B1'].set_ylabel('Similarity index')
 axd['B1'].yaxis.set_major_formatter(matplotlib.ticker.FormatStrFormatter('%.2f'))
+axd['B1'].set_xticklabels([])
 axd['B1'].set_title(f'Similarity index', style='italic')
 
 ### B2 ###
 sem_high = np.std(high, axis=0) / np.sqrt(len(subjects))
 sem_low = np.std(low, axis=0) / np.sqrt(len(subjects))
-sig_span(axd['B2'], sig, times)
+sig_span(axd['B2'], sig, times, lab=False)
 axd['B2'].plot(times, high.mean(0), label='Pattern', alpha=1)
 axd['B2'].plot(times, low.mean(0), label='Random', alpha=1)
 axd['B2'].fill_between(times, high.mean(0) - sem_high, high.mean(0) + sem_high, alpha=0.2)
 axd['B2'].fill_between(times, low.mean(0) - sem_low, low.mean(0) + sem_low, alpha=0.2)
 # axd['B2'].fill_between(times, high.mean(0) + sem_high, low.mean(0) - sem_low, where=sig, alpha=0.3, label='Significant')
-axd['B2'].legend()
+# axd['B2'].legend(["_", 'Pattern', 'Random'])
+axd['B2'].legend(frameon=False)
 axd['B2'].set_ylabel('cvMahalanobis')
+# axd['B2'].set_xticklabels([])
 axd['B2'].set_title(f'cvMahalanobis of Random and Pattern trials', style='italic')
 
 ### D1 ###
@@ -120,6 +148,7 @@ axd['D1'].fill_between(times, rhos.mean(0) - sem, rhos.mean(0) + sem, alpha=0.2)
 # axd['D1'].fill_between(times, 0, rhos.mean(0), where=sig, alpha=.3, label='Corrected')
 axd['D1'].axhline(0, color='black', linestyle='dashed')
 axd['D1'].set_ylabel("Spearman's rho")
+axd['D1'].set_xticklabels([])
 axd['D1'].set_title(f'Within subject block correlation', style='italic')
 
 ### D2 ###
@@ -137,8 +166,6 @@ axd['D2'].axhline(0, color="black", linestyle="dashed")
 axd['D2'].set_ylabel("Spearman's rho")
 axd['D2'].set_xlabel('Time (s)')
 axd['D2'].set_title(f'Within subject learning index correlation', style='italic')
-for ax in ['B1', 'B2', 'D1']:
-    axd[ax].sharex(axd['D2'])
 
 ### C ###
 idx_times = np.where((times >= 0.3) & (times <= 0.5))[0]
@@ -160,6 +187,8 @@ axd['C'].set_xticks(range(5))
 axd['C'].set_xticklabels([f'Session {i}' for i in range(5)])
 axd['C'].set_ylabel("Spearman's rho")
 axd['C'].set_title(f'Correlation with learning index', style='italic')
+# handles, labels = axd['B1'].get_legend_handles_labels()
+# fig.legend(handles, labels, loc='upper right', title='Conditions')
 
 plt.savefig(figures_dir /  f"{lock}.pdf", transparent=True)
 plt.close()
