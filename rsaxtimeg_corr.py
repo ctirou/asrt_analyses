6f7e849764e7355523f7fb1e55e7f9fa8119c3dc
ctirou <102290014+ctirou@users.noreply.github.com>
2025-05-21 16:23:47 +0200

    timeg and corr final 💊

--- rsaxtimeg_corr.py
+++ rsaxtimeg_corr.py
@@ -9,162 +9,88 @@ from base import *
 from config import *
 
 data_path = DATA_DIR
-subjects, epochs_list = SUBJS, EPOCHS
+subjects, epochs_list = SUBJS15, EPOCHS
 metric = 'mahalanobis'
 
 data = 'cv'
 analysis = 'pat_high_rdm_high'
 lock = 'stim'
 
-figures_dir = FIGURES_DIR / 'time_gen' / "source" / lock
+figures_dir = FIGURES_DIR / 'time_gen' / "source"
 
 # get times
-times = np.load(data_path / "times.npy")
+times = np.linspace(-0.2, 0.6, 82)
 timesg = np.linspace(-1.5, 1.5, 305)
 
 # correlation between rsa and time generalization
 idx_rsa = np.where((times >= .3) & (times <= .5))[0]
-idx_rsa = np.load("/Users/coum/MEGAsync/figures/RSA/sensors/sig_rsa.npy")
 idx_timeg = np.where((timesg >= -0.5) & (timesg < 0))[0]
 
-analysis = "tg_rdm_emp"
-res_dir = TIMEG_DATA_DIR / analysis / lock
+res_dir = RESULTS_DIR / 'TIMEG' / 'source'
+networks = NETWORKS + ['Cerebellum-Cortex']
+network_names = NETWORK_NAMES + ['Cerebellum']
 
-networks = NETWORKS
-network_names = NETWORK_NAMES
-
-src_pat, src_rand = {}, {}
+patterns, randoms = {}, {}
 for network in tqdm(networks):
-    if not network in src_pat:
-        src_pat[network], src_rand[network] = [], []
+    if not network in patterns:
+        patterns[network], randoms[network] = [], []
+    all_pat, all_rand, all_diag = [], [], []
     patpat, randrand = [], []
     for i, subject in enumerate(subjects):
         pat, rand = [], []
         for j in [0, 1, 2, 3, 4]:
-            pat.append(np.load(res_dir / network / 'pattern' / f"{subject}-{j}-scores.npy"))
-            rand.append(np.load(res_dir / network / 'random' / f"{subject}-{j}-scores.npy"))
+            p = np.load(res_dir / network / 'scores_skf' / subject / f"pat-{j}.npy")
+            r = np.load(res_dir / network / 'scores_skf' / subject / f"rand-{j}.npy")
+            pat.append(p)
+            rand.append(r)
         patpat.append(np.array(pat))
         randrand.append(np.array(rand))
-    src_pat[network] = np.array(patpat)
-    src_rand[network] = np.array(randrand)
+    patterns[network] = np.array(patpat)
+    randoms[network] = np.array(randrand)
 
+# Load RSA data
 all_highs, all_lows = {}, {}
 diff_sess = {}
 for network in tqdm(networks):
-    if not network in diff_sess:
-        all_highs[network] = []
-        all_lows[network] = []
+    if not network in all_highs:
+        all_highs[network], all_lows[network] = [], []
         diff_sess[network] = []
     for subject in subjects:        
-        # # RSA stuff
-        behav_dir = op.join(RAW_DATA_DIR, "%s/behav_data/" % (subject))
+        # RSA stuff
+        behav_dir = op.join(HOME / 'raw_behavs' / subject)
         sequence = get_sequence(behav_dir)
-        res_path = RESULTS_DIR / "RSA" / 'source' / network / lock / 'rdm' / subject
-        high, low = get_all_high_low(res_path, sequence, analysis, cv=True)    
-        all_highs[network].append(high)    
+        res_path = RESULTS_DIR / 'RSA' / 'source' / network / 'rdm_skf' / subject
+        pats, rands = [], []
+        for epoch_num in range(5):
+            pats.append(np.load(res_path / f"pat-{epoch_num}.npy"))
+            rands.append(np.load(res_path / f"rand-{epoch_num}.npy"))
+        pats = np.array(pats)
+        rands = np.array(rands)
+        high, low = get_all_high_low(pats, rands, sequence, False)
+        high, low = np.array(high).mean(0), np.array(low).mean(0)
+        if subject == 'sub05':
+            pat_b1 = np.load(res_path / "pat-b1.npy")
+            high[0] = pat_b1.copy()
+            rand_b1 = np.load(res_path / "rand-b1.npy")
+            low[0] = rand_b1.copy()
+        all_highs[network].append(high)
         all_lows[network].append(low)
     all_highs[network] = np.array(all_highs[network])
     all_lows[network] = np.array(all_lows[network])
     # plot diff session by session
     for i in range(5):
-        rev_low = all_lows[network][:, :, i, :].mean(1) - all_lows[network][:, :, 0, :].mean(axis=1)
-        rev_high = all_highs[network][:, :, i, :].mean(1) - all_highs[network][:, :, 0, :].mean(axis=1)
-        diff_sess[network].append(rev_low - rev_high)
+        low_sess = all_lows[network][:, i, :] - all_lows[network][:, 0, :]
+        high_sess = all_highs[network][:, i, :] - all_highs[network][:, 0, :]
+        diff_sess[network].append(low_sess - high_sess)
     diff_sess[network] = np.array(diff_sess[network]).swapaxes(0, 1)
-learn_index_df = pd.read_csv(FIGURES_DIR / 'behav' / 'learning_indices.csv', sep="\t", index_col=0)
-
-fig, axes = plt.subplots(1, 9, sharey=True, figsize=(20, 5), layout='tight')
-for i, network in enumerate(networks):
-    rsa = diff_sess[network].copy()[:, :, idx_rsa].mean(2)
-    contrasts = src_pat[network] - src_rand[network]
-    timeg = []
-    for sub in range(len(subjects)):
-        tg = []
-        for j in range(5):
-            tg.append(contrasts[sub, j, idx_timeg][:, idx_timeg].mean())
-        timeg.append(np.array(tg))
-    timeg = np.array(timeg)
-    slopes, intercepts = [], []
-    for sub, subject in enumerate(subjects):
-        slope, intercept = np.polyfit(timeg[sub], rsa[sub], 1)
-        axes[i].scatter(timeg[sub], rsa[sub], alpha=0.3)
-        axes[i].plot(timeg[sub], slope * timeg[sub] + intercept, alpha=0.6, label=f"Subject {sub+1}")
-        slopes.append(slope)
-        intercepts.append(intercept)
-    timeg_range = np.linspace(timeg.min(), timeg.max(), 100)
     
-    axes[i].plot(timeg_range, np.mean(slopes) * timeg_range + np.mean(intercepts), color='black', lw=4, label='Mean fit')
-    axes[i].set_xlabel('Mean Time generalization', fontsize=12)
-    # axes[i].legend(frameon=False, ncol=2)
-    axes[i].spines['top'].set_visible(False)
-    axes[i].spines['right'].set_visible(False)
-    axes[i].set_title(network_names[i])
-    if i == 0:
-        axes[i].set_ylabel('Similarity index', fontsize=12)
-    rhos = []
-    for sub in range(len(subjects)):
-        r, p = spear(timeg[sub], rsa[sub])
-        rhos.append(r)        
-    pval = ttest_1samp(rhos, 0)[1]
-    axes[i].set_title(f"{network_names[i]}")
-    
-    axes[i].text(0.05, 0.95, f"$p=${pval:.2f}", transform=axes[i].transAxes, fontsize=12, verticalalignment='top')
-    
-fig.suptitle("Correlation between mean predictive activity and mean representational similarity", fontweight='bold',  fontsize=16)
-
-fig.savefig(figures_dir / f"rsa_corr.pdf", transparent=True)
-plt.close()
-
-
-fig, axes = plt.subplots(1, 9, sharey=True, figsize=(20, 5), layout='tight')
-for i, network in enumerate(networks):
-    contrasts = src_pat[network] - src_rand[network]
-    timeg = []
-    for sub in range(len(subjects)):
-        tg = []
-        for j in range(5):
-            tg.append(contrasts[sub, j, idx_timeg][:, idx_timeg].mean())
-        timeg.append(np.array(tg))
-    timeg = np.array(timeg)
-    slopes, intercepts = [], []
-    for sub, subject in enumerate(subjects):
-        slope, intercept = np.polyfit(timeg[sub], learn_index_df.iloc[sub], 1)
-        axes[i].scatter(timeg[sub], learn_index_df.iloc[sub], alpha=0.3)
-        axes[i].plot(timeg[sub], slope * timeg[sub] + intercept, alpha=0.6, label=f"Subject {sub+1}")
-        slopes.append(slope)
-        intercepts.append(intercept)
-
-    timeg_range = np.linspace(timeg.min(), timeg.max(), 100)
-
-    axes[i].plot(timeg_range, np.mean(slopes) * timeg_range + np.mean(intercepts), color='black', lw=4, label='Mean fit')
-    # axes[i].set_xlabel('Mean time generalization', fontsize=12)
-    # axes[i].legend(frameon=False, ncol=2)
-    axes[i].spines['top'].set_visible(False)
-    axes[i].spines['right'].set_visible(False)
-    axes[i].set_title(network_names[i])
-    if i == 0:
-        axes[i].set_ylabel('Learning index', fontsize=12)
-    learn_index_flat = learn_index_df.to_numpy().flatten()
-
-    rhos = []
-    for sub in range(len(subjects)):
-        r, p = spear(timeg[sub], rsa[sub])
-        rhos.append(r)        
-    pval = ttest_1samp(rhos, 0)[1]
-    axes[i].set_title(f"{network_names[i]}")
-    
-    axes[i].text(0.05, 0.95, f"$p=${pval:.2f}", transform=axes[i].transAxes, fontsize=12, verticalalignment='top')
-
-fig.suptitle("Correlation between mean predictive activity and learning", fontweight='bold', fontsize=16)
-
-fig.savefig(figures_dir / f"learn_corr.pdf", transparent=True)
-plt.close()
+learn_index_df = pd.read_csv(FIGURES_DIR / 'behav' / 'learning_indices15.csv', sep="\t", index_col=0)
 
 plt.rcParams.update({'font.size': 10, 'font.family': 'serif', 'font.serif': 'Arial'})
 
-fig, axes = plt.subplots(2, 9, sharey=False, sharex=True, figsize=(20, 5), layout='tight')
+fig, axes = plt.subplots(2, 10, sharey=False, sharex=True, figsize=(20, 5), layout='tight')
 for i, network in enumerate(networks):
-    contrasts = src_pat[network] - src_rand[network]
+    contrasts = patterns[network] - randoms[network]
     timeg = []
     for sub in range(len(subjects)):
         tg = []
@@ -235,23 +161,19 @@ for i, network in enumerate(networks):
         axes[1, i].set_yticklabels([])
 
 fig.suptitle("Correlation between mean predictive activity and learning (top) and similarity index (bottom)", fontweight='bold', fontsize=16)
-
-# fig.text(0.5, 0.02, 'Average pre-stimulus time generalization contrast', ha='center', fontsize=12)
-
-fig.savefig(figures_dir / f"combined_corr.pdf", transparent=True)
+fig.savefig(figures_dir / f"combined_corr-all.pdf", transparent=True)
 plt.close()
 
-networks = ['SomMot', 'DorsAttn', 'Cont', 'Default']
-network_names = ['Somatomotor', 'Dorsal Attention', 'Control', 'Default']
-fig, axes = plt.subplots(2, 4, sharey=False, sharex=True, figsize=(10, 7), layout='tight')
+networks = ['SomMot', 'DorsAttn', 'SalVentAttn', 'Cont', 'Default', 'Cerebellum-Cortex']
+network_names = ['Somatomotor', 'Dorsal Attention', 'Ventral Attention', 'Control', 'Default', 'Cerebellum']
+fig, axes = plt.subplots(2, 6, sharey=False, sharex=True, figsize=(12, 7), layout='tight')
 for i, network in enumerate(networks):
-    contrasts = src_pat[network] - src_rand[network]
+    contrasts = patterns[network] - randoms[network]
     timeg = []
     for sub in range(len(subjects)):
         tg = []
         for j in range(5):
-            # tg.append(contrasts[sub, j, idx_timeg][:, idx_timeg].mean())
-            tg.append(np.diag(contrasts[sub, j, idx_timeg][:, idx_timeg]).mean())
+            tg.append(contrasts[sub, j, idx_timeg][:, idx_timeg].mean())
         timeg.append(np.array(tg))
     timeg = np.array(timeg)
     slopes, intercepts = [], []
@@ -316,7 +238,5 @@ for i, network in enumerate(networks):
 
 fig.suptitle("Correlation between mean predictive activity and learning (top)\nand mean representational change effect (bottom)", fontweight='bold', fontsize=16)
 
-# fig.text(0.5, 0.02, 'Average pre-stimulus time generalization contrast', ha='center', fontsize=12)
-
-fig.savefig(figures_dir / f"combined_corr_best-2.pdf", transparent=True)
+fig.savefig(figures_dir / f"combined_corr_best.pdf", transparent=True)
 plt.close()
