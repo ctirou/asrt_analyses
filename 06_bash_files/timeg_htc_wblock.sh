c317bd7df5e046365e11cdb341d50d87e8f688be
ctirou <102290014+ctirou@users.noreply.github.com>
2026-02-25 23:36:15 +0100

    ressource update in bash files 🚀

--- 06_bash_files/timeg_htc_wblock.sh
+++ 06_bash_files/timeg_htc_wblock.sh
@@ -7,8 +7,8 @@
 
 #SBATCH --partition=htc
 #SBATCH --cpus-per-task=20              # Run a single task (by default tasks == CPU)
-#SBATCH --mem=15G                    # Memory in MB by default
-#SBATCH --time=0-01:00:00                    # 7 days by default on htc partition
+#SBATCH --mem=50G                    # Memory in MB by default
+#SBATCH --time=0-03:00:00                    # 7 days by default on htc partition
 #SBATCH --array=0-14                    # number of subjects (last num included)
 
 # Commands to be submitted:
