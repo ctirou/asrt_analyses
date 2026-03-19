82f705b69938538343e752b85f62f935a6bbfe97
ctirou <102290014+ctirou@users.noreply.github.com>
2026-03-19 18:44:54 +0100

    figures for merged subregs, online offline detailed plot 🎢

--- 01_behavior/online_offline.py
+++ 01_behavior/online_offline.py
@@ -5,13 +5,16 @@ import os
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from scipy.ndimage import gaussian_filter1d
+from base import *
 from config import *
 from tqdm.auto import tqdm
+import os.path as op
 
 subjects = SUBJS15
 path_data = HOME / 'raw_behavs'
 figures_dir = FIGURES_DIR
-saving = True
+saving = False
 
 sessions = ['0', '1', '2', '3', '4']
 blocks = np.arange(1, 24)
@@ -82,41 +85,41 @@ plt.rcParams.update({'font.family': 'serif', 'font.serif': 'Arial'})
 color1 = "#FFD966"
 color2 = "#FF718A"
 
-fig, ax = plt.subplots(1, 2, figsize=(7, 3), sharey=True, layout='tight')
-for axx in ax.flatten():
-    axx.spines['top'].set_visible(False)
-    axx.spines['right'].set_visible(False)
+# fig, ax = plt.subplots(1, 2, figsize=(7, 3), sharey=True, layout='tight')
+# for axx in ax.flatten():
+#     axx.spines['top'].set_visible(False)
+#     axx.spines['right'].set_visible(False)
 
-# online
-sem_rand = np.nanstd(online_random, axis=0) / np.sqrt(n)
-ax[0].plot(blocks, np.nanmean(online_random, axis=0), color=color2, label='Random', zorder=9, alpha=1)
-ax[0].fill_between(blocks, np.nanmean(online_random, axis=0) - sem_rand, np.nanmean(online_random, axis=0) + sem_rand, color=color2, alpha=0.1)
+# # online
+# sem_rand = np.nanstd(online_random, axis=0) / np.sqrt(n)
+# ax[0].plot(blocks, np.nanmean(online_random, axis=0), color=color2, label='Random', zorder=9, alpha=1)
+# ax[0].fill_between(blocks, np.nanmean(online_random, axis=0) - sem_rand, np.nanmean(online_random, axis=0) + sem_rand, color=color2, alpha=0.1)
 
-sem_pat = np.nanstd(online_pattern, axis=0) / np.sqrt(n)
-ax[0].plot(blocks, np.nanmean(online_pattern, axis=0), color=color1, label='Pattern', zorder=10, alpha=1)
-ax[0].fill_between(blocks, np.nanmean(online_pattern, axis=0) - sem_pat, np.nanmean(online_pattern, axis=0) + sem_pat, color=color1, alpha=0.2)
+# sem_pat = np.nanstd(online_pattern, axis=0) / np.sqrt(n)
+# ax[0].plot(blocks, np.nanmean(online_pattern, axis=0), color=color1, label='Pattern', zorder=10, alpha=1)
+# ax[0].fill_between(blocks, np.nanmean(online_pattern, axis=0) - sem_pat, np.nanmean(online_pattern, axis=0) + sem_pat, color=color1, alpha=0.2)
 
-ax[0].set_title('Online', fontstyle='italic')
-ax[0].set_xticks(blocks[::2])
-ax[0].set_xlabel('Block')
-ax[0].set_ylabel(f'RT Difference (a.u.) - {n_bin} bins')
-ax[0].grid(alpha=0.2, linestyle='--', color='gray', linewidth=0.5)
+# ax[0].set_title('Online', fontstyle='italic')
+# ax[0].set_xticks(blocks[::2])
+# ax[0].set_xlabel('Block')
+# ax[0].set_ylabel(f'RT Difference (a.u.) - {n_bin} bins')
+# ax[0].grid(alpha=0.2, linestyle='--', color='gray', linewidth=0.5)
 
-# offline
-sem_rand = np.nanstd(offline_random, axis=0) / np.sqrt(n)
-ax[1].plot(blocks[1:], np.nanmean(offline_random, axis=0), color=color2, zorder=10, alpha=1)
-ax[1].fill_between(blocks[1:], np.nanmean(offline_random, axis=0) - sem_rand, np.nanmean(offline_random, axis=0) + sem_rand, color=color2, alpha=0.1)
+# # offline
+# sem_rand = np.nanstd(offline_random, axis=0) / np.sqrt(n)
+# ax[1].plot(blocks[1:], np.nanmean(offline_random, axis=0), color=color2, zorder=10, alpha=1)
+# ax[1].fill_between(blocks[1:], np.nanmean(offline_random, axis=0) - sem_rand, np.nanmean(offline_random, axis=0) + sem_rand, color=color2, alpha=0.1)
 
-sem_pat = np.nanstd(offline_pattern, axis=0) / np.sqrt(n)
-ax[1].plot(blocks[1:], np.nanmean(offline_pattern, axis=0), color=color1, zorder=9, alpha=1)
-ax[1].fill_between(blocks[1:], np.nanmean(offline_pattern, axis=0) - sem_pat, np.nanmean(offline_pattern, axis=0) + sem_pat, color=color1, alpha=0.2)
-ax[1].set_title('Offline', fontstyle='italic')
-ax[1].set_xticks(blocks[1::2])
-ax[1].set_xlabel('Block')
-ax[1].grid(alpha=0.2, linestyle='--', color='gray', linewidth=0.5)
-if saving: 
-    fig.savefig(figures_dir / 'online_offline_learning.png', dpi=300)
-    plt.close()
+# sem_pat = np.nanstd(offline_pattern, axis=0) / np.sqrt(n)
+# ax[1].plot(blocks[1:], np.nanmean(offline_pattern, axis=0), color=color1, zorder=9, alpha=1)
+# ax[1].fill_between(blocks[1:], np.nanmean(offline_pattern, axis=0) - sem_pat, np.nanmean(offline_pattern, axis=0) + sem_pat, color=color1, alpha=0.2)
+# ax[1].set_title('Offline', fontstyle='italic')
+# ax[1].set_xticks(blocks[1::2])
+# ax[1].set_xlabel('Block')
+# ax[1].grid(alpha=0.2, linestyle='--', color='gray', linewidth=0.5)
+# if saving: 
+#     fig.savefig(figures_dir / 'online_offline_learning.png', dpi=300)
+#     plt.close()
     
 fig, ax = plt.subplots(4, 2, figsize=(7, 10), sharey=True, sharex=False, layout='tight')
 for axx in ax.flatten():
@@ -191,3 +194,163 @@ ax[3, 1].set_xlabel('Block')
 if saving:
     fig.savefig(figures_dir / 'online_offline_learning_detailed.png', dpi=300)
     plt.close()
+    
+# RS offline
+data_type = "rdm_blocks" 
+all_pats, all_rands = [], []
+all_pats_blocks, all_rands_blocks = [], []
+for subject in tqdm(subjects):
+    res_path = RESULTS_DIR / 'RSA' / 'sensors' / data_type / subject
+    # read behav        
+    behav_dir = op.join(HOME / 'raw_behavs' / subject)
+    sequence = get_sequence(behav_dir)
+    pattern_blocks, random_blocks = [], []
+    for epoch_num in range(5):
+        blocks = [i for i in range(1, 4)] if epoch_num == 0 else [i for i in range(5 * (epoch_num - 1) + 1, epoch_num * 5 + 1)]
+        pats, rands = [], []
+        for block in blocks:
+            pattern_blocks.append(np.load(res_path / f"pat-{epoch_num}-{block}.npy"))
+            random_blocks.append(np.load(res_path / f"rand-{epoch_num}-{block}.npy"))
+    if subject == 'sub05':
+        pat_bsl = np.load(res_path / "pat-1-1.npy")
+        rand_bsl = np.load(res_path / "rand-1-1.npy")
+        for i in range(3):
+            pattern_blocks[i] = pat_bsl.copy()
+            random_blocks[i] = rand_bsl.copy()
+    pattern_blocks = np.array(pattern_blocks)
+    random_blocks = np.array(random_blocks)
+    high, low = get_all_high_low(pattern_blocks, random_blocks, sequence, False)
+    all_pats.append(high.mean(0))
+    all_rands.append(low.mean(0))
+all_pats = np.array(all_pats)
+all_rands = np.array(all_rands)
+bsl_pat = np.nanmean(all_pats[:, :3, :], 1) 
+bsl_rand = np.nanmean(all_rands[:, :3, :], 1)
+pat = all_pats - bsl_pat[:, np.newaxis, :]
+rand = all_rands - bsl_rand[:, np.newaxis, :]
+diff_rp = rand - pat
+# extract index from GAMM segments
+segments = pd.read_csv(FIGURES_DIR / "TM" / "segments_tr_sensors.csv")
+start = segments.iloc[0]['start'] if data_type.endswith("new") else segments.iloc[1]['start']
+end = segments.iloc[0]['end'] if data_type.endswith("new") else segments.iloc[1]['end']
+idx = np.arange(int(start), int(end)+1)
+# block-wise mean for the significant time window
+diff_rp_blocks = np.nanmean(diff_rp[:, :, idx], axis=(-1)) # shape (n_subjects, n_blocks)
+
+offline_rsa = np.diff(diff_rp_blocks, axis=1) # shape (n_subjects, n_blocks - 1)
+
+# PA offline
+# --- Temporal generalization sensors --- blocks ---
+data_type = 'scores_blocks'
+subjects = SUBJS15
+times = np.linspace(-4, 4, 813)
+pats_blocks, rands_blocks = [], []
+for subject in tqdm(subjects):
+    res_path = RESULTS_DIR / 'TIMEG' / 'sensors' / data_type / subject
+    pattern, random = [], []
+    for block in range(1, 24):
+        pfname = res_path / f'pat-{block}.npy' if block not in [1, 2, 3] else res_path / f'pat-0-{block}.npy'
+        rfname = res_path / f'rand-{block}.npy' if block not in [1, 2, 3] else res_path / f'rand-0-{block}.npy'
+        pattern.append(np.load(pfname))
+        random.append(np.load(rfname))
+    if subject == 'sub05':
+        pat_bsl = np.load(res_path / "pat-4.npy")
+        rand_bsl = np.load(res_path / "rand-4.npy")
+        for i in range(3):
+            pattern[i] = pat_bsl.copy()
+            random[i] = rand_bsl.copy()
+    pats_blocks.append(np.array(pattern))
+    rands_blocks.append(np.array(random))
+pats_blocks = np.array(pats_blocks)
+rands_blocks = np.array(rands_blocks)
+conts_blocks = pats_blocks - rands_blocks
+# mean box
+idx_timeg = np.where((times >= -0.5) & (times < 0))[0]
+box_blocks = []
+for sub in range(len(subjects)):
+    tg = []
+    dg = []
+    for block in range(23):
+        data = conts_blocks[sub, block, idx_timeg, :][:, idx_timeg]
+        tg.append(data.mean())
+    box_blocks.append(np.array(tg))
+box_blocks = np.array(box_blocks)
+
+offline_pa = np.diff(box_blocks, axis=1)
+
+# plot all offlines
+
+blocks = np.arange(1, 24)
+plt.rcParams.update({'font.family': 'serif', 'font.serif': 'Arial'})
+
+fig, ax = plt.subplots(3, 1, figsize=(5, 12), sharey=False, layout='tight')
+for axx in ax.flatten():
+    axx.spines['top'].set_visible(False)
+    axx.spines['right'].set_visible(False)
+    axx.grid(alpha=0.2, linestyle='--', color='gray', linewidth=0.5)
+    axx.grid(True, which='minor', alpha=0.1, linestyle=':', color='gray', linewidth=0.3)
+    axx.set_xticks(blocks[::2])
+    axx.axhline(0, color='gray', alpha=0.5, linestyle='-', linewidth=1)
+    axx.axvspan(1, 3, color='gray', alpha=0.1)
+# PA
+mean_pa = np.nanmean(offline_pa, axis=0)
+sem = np.nanstd(offline_pa, axis=0) / np.sqrt(n)
+ax[0].set_title('PA', fontstyle='italic')
+ax[0].plot(blocks[1:], mean_pa, color='blue', zorder=9, alpha=0.3)
+ax[0].plot(blocks[1:], gaussian_filter1d(mean_pa, sigma=2), color='blue', zorder=10, alpha=1)
+ax[0].fill_between(blocks[1:], mean_pa - sem, mean_pa + sem, color='blue', alpha=0.1)
+# Behavioral
+contrast_offline = offline_pattern - offline_random
+mean_beh = np.nanmean(contrast_offline, axis=0)
+sem = np.nanstd(contrast_offline, axis=0) / np.sqrt(n)
+ax[1].set_title('Behavior', fontstyle='italic')
+ax[1].plot(blocks[1:], mean_beh, color='purple', zorder=11, alpha=0.3)
+ax[1].plot(blocks[1:], gaussian_filter1d(mean_beh, sigma=2), color='purple', zorder=12, alpha=1)
+ax[1].fill_between(blocks[1:], mean_beh - sem, mean_beh + sem, color='purple', alpha=0.2)
+# RSA
+mean_rsa = np.nanmean(offline_rsa, axis=0)
+sem = np.nanstd(offline_rsa, axis=0) / np.sqrt(n)
+ax[2].set_title('RS', fontstyle='italic')
+ax[2].plot(blocks[1:], mean_rsa, color='green', zorder=10, alpha=0.3)
+ax[2].plot(blocks[1:], gaussian_filter1d(mean_rsa, sigma=2), color='green', zorder=11, alpha=1)
+ax[2].fill_between(blocks[1:], mean_rsa - sem, mean_rsa + sem, color='green', alpha=0.2)
+ax[2].set_xlabel('Block')
+
+# compute correlation between PA and behavior
+from scipy.stats import spearmanr as spear
+
+all_rhos_pa = np.array([[spear(contrast_offline[sub], offline_pa[sub])[0]] for sub in range(len(subjects))])
+all_rhos_pa, _, _ = fisher_z_and_ttest(all_rhos_pa)
+
+correlations_pa = []
+for block in range(22):
+    r, p = spearmanr(offline_pa[:, block], contrast_offline[:, block])
+    correlations_pa.append((r, p))
+correlations_pa = np.array(correlations_pa)
+
+# compute correlation between RSA and behavior
+correlations_rsa = []
+for block in range(22):
+    r, p = spearmanr(offline_rsa[:, block], contrast_offline[:, block])
+    correlations_rsa.append((r, p))
+correlations_rsa = np.array(correlations_rsa)
+
+fig, ax = plt.subplots(1, 2, figsize=(10, 5), sharey=True, layout='tight')
+for axx in ax.flatten():
+    axx.spines['top'].set_visible(False)
+    axx.spines['right'].set_visible(False)
+    axx.grid(alpha=0.2, linestyle='--', color='gray', linewidth=0.5)
+    axx.grid(True, which='minor', alpha=0.1, linestyle=':', color='gray', linewidth=0.3)
+    axx.set_xticks(blocks[1::2])
+
+# PA vs behavior
+ax[0].set_title('PA vs Behavior', fontstyle='italic')
+ax[0].plot(blocks[1:], correlations_pa[:, 0], color='blue', zorder=10, alpha=1)
+ax[0].fill_between(blocks[1:], correlations_pa[:, 0] - correlations_pa[:, 1], correlations_pa[:, 0] + correlations_pa[:, 1], color='blue', alpha=0.1)
+
+# RSA vs behavior
+ax[1].set_title('RSA vs Behavior', fontstyle='italic')
+ax[1].plot(blocks[1:], correlations_rsa[:, 0], color='green', zorder=10, alpha=1)
+ax[1].fill_between(blocks[1:], correlations_rsa[:, 0] - correlations_rsa[:, 1], correlations_rsa[:, 0] + correlations_rsa[:, 1], color='green', alpha=0.1)
+
+ax[1].set_xlabel('Block')
---
+++ 04_source/time_gen/timeg_net_subreg_merged_plot.py
@@ -0,0 +1,167 @@
+# Authors: Coumarane Tirou <c.tirou@hotmail.com>
+# License: BSD (3-clause)
+
+from base import *
+from config import *
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import re
+from mne import read_labels_from_annot
+from tqdm.auto import tqdm
+from base import decod_stats
+
+subjects, subjects_dir = SUBJS15, FREESURFER_DIR
+figures_dir = ensured(FIGURES_DIR / "time_gen" / "source")
+analysis = 'scores_blocks_subreg_merged'
+networks = ['SomMot', 'DorsAttn']
+parc = "Schaefer2018_200Parcels_7Networks"
+hemi = 'both'
+use_decode = False
+
+def natural_sort_key(name):
+    return [int(n) for n in re.findall(r'\d+', name)]
+
+def get_base_name(label_name):
+    """Strip hemisphere prefix (LH_/RH_) and suffix (-lh/-rh) to get bilateral base name."""
+    name = label_name.replace('_LH_', '_').replace('_RH_', '_')
+    return name.replace('-lh', '').replace('-rh', '')
+
+times = np.linspace(-1, 1.5, 256)
+net_cmap = {
+    'SomMot': '#DE8F05',
+    'DorsAttn': '#029E73',
+}
+
+net_labels_map = {
+    'SomMot': 'Sensorimotor',
+    'DorsAttn': 'Dorsal Attention',
+}
+
+plt.rcParams.update({'font.size': 10, 'font.family': 'serif', 'font.serif': 'Arial'})
+
+if not use_decode:
+    # get significant time points from GAMM csv
+    seg_df = pd.read_csv(FIGURES_DIR / "TM" / "em_segments_pa_tr_source_subreg_merged.csv")
+    seg_df = seg_df[seg_df['metric'].str.contains('PA')]
+    sig_dict = {}
+    for _, row in seg_df.iterrows():
+        arr = sig_dict.get(row["label"], np.zeros(len(times), dtype=bool))
+        arr[row["start"]:row["end"] + 1] = True
+        sig_dict[row["label"]] = arr
+    sig_df = pd.read_csv(FIGURES_DIR / "TM" / "smooth_pa_tr_source_subreg_merged.csv")
+    sig_df = sig_df[sig_df['metric'].str.contains('PA')]
+    for i, net in enumerate(sig_df['label'].unique()):
+        if net in sig_dict:
+            if sig_df[sig_df['label'] == net]['signif_holm'][i] == 'ns':
+                del sig_dict[net]
+
+for network in networks:
+
+    # Get label names matching the directory names produced by timeg_net_subreg_merged.py:
+    # merged lh+rh pairs use the base name; singletons (one hemi only) keep their original name.
+    raw_labels = read_labels_from_annot(
+        subject=subjects[0], parc=parc, hemi=hemi,
+        subjects_dir=subjects_dir, regexp=network, verbose='error'
+    )
+    label_groups = {}
+    for label in raw_labels:
+        base = get_base_name(label.name)
+        label_groups.setdefault(base, []).append(label.name)
+    label_names = sorted(
+        [base if len(names) == 2 else names[0] for base, names in label_groups.items()],
+        key=natural_sort_key
+    )
+
+    # Load timeg data per label: contrasts[lname] -> (n_subjects, n_blocks, n_times)
+    contrasts = {lname: [] for lname in label_names}
+
+    for subject in tqdm(subjects):
+        res_path = RESULTS_DIR / 'TIMEG' / 'source' / network / analysis / subject
+
+        for lname in label_names:
+            pattern_blocks, random_blocks = [], []
+
+            for block in range(4, 24):
+                pattern_blocks.append(np.load(res_path / lname / f'pat-{block}.npy'))
+                random_blocks.append(np.load(res_path / lname / f'rand-{block}.npy'))
+
+            pattern_blocks = np.array(pattern_blocks) * 100
+            random_blocks = np.array(random_blocks) * 100
+
+            contrasts[lname].append(pattern_blocks - random_blocks)
+
+    for lname in label_names:
+        contrasts[lname] = np.array(contrasts[lname])  # (n_subjects, n_blocks, n_times)
+
+    color = net_cmap[network]
+    chance = 0
+
+    n_rows = len(label_names)
+    n_cols = 1
+
+    fig, axes = plt.subplots(n_rows, n_cols, figsize=(4.5, n_rows * 1.75),
+                             sharey=True, sharex=True, layout="tight")
+    axes = np.array(axes).reshape(n_rows, n_cols)
+
+    for row, lname in enumerate(label_names):
+        ax = axes[row, 0]
+
+        data = contrasts[lname].mean(1)  # (n_subjects, n_times)
+        mean = data.mean(0)
+        sem = np.std(data, axis=0) / np.sqrt(len(subjects))
+
+        if not use_decode:
+            sig = sig_dict[lname] if lname in sig_dict else np.zeros(len(times), dtype=bool)
+            sig_level_rows = sig_df[sig_df['label'] == lname]['signif_holm']
+            if len(sig_level_rows) > 0 and sig_level_rows.values[0] != 'ns':
+                ax.text(0.4, 0.1, sig_level_rows.values[0], fontsize=20,
+                        ha='center', va='bottom', color=color, weight='bold')
+        else:
+            pval = decod_stats(data, -1)
+            sig = pval < 0.05
+
+        ax.spines['top'].set_visible(False)
+        ax.spines['right'].set_visible(False)
+        ax.axvspan(0, 0.2, facecolor='grey', edgecolor=None, alpha=.1)
+        ax.axhline(chance, color='grey', alpha=.5)
+
+        ax.plot(times, mean, alpha=1, zorder=10, color='C7')
+        for start, end in contiguous_regions(sig):
+            ax.plot(times[start:end], mean[start:end], alpha=1, zorder=10, color=color)
+
+
+        ax.fill_between(times, mean - sem, mean + sem, alpha=0.2, zorder=5, facecolor='C7')
+        ax.fill_between(times, mean - sem, mean + sem, where=sig, alpha=0.5, zorder=5, color=color)
+        ax.fill_between(times, mean - sem, chance, where=sig, alpha=0.3, zorder=5, facecolor=color)
+
+        # Short region label: strip network prefix and hemisphere suffix
+        parts = lname.split(f'{network}_')
+        region_label = parts[-1].replace('-lh', '').replace('-rh', '') if len(parts) > 1 else lname
+        ax.text(0.05, 0.95, region_label, transform=ax.transAxes,
+                fontsize=9, va='top', ha='left', color='k', fontstyle='italic')
+
+        ax.set_ylabel('D.i.A. (a.u.)', fontsize=9)
+        if row == n_rows - 1:
+            ax.set_xlabel('Time (s)', fontsize=9)
+            ax.tick_params(labelbottom=True)
+
+    fig.suptitle(net_labels_map[network], fontsize=13, color=color)
+    
+    fig.savefig(figures_dir / f"timeg-net-subreg-merged-{network}.pdf")
+    plt.close()
+
+    # Save table per network with label column
+    rows = []
+    for lname in label_names:
+        data = contrasts[lname].mean(1)  # (n_subjects, n_times)
+        for j, subject in enumerate(subjects):
+            for t, time in enumerate(times):
+                rows.append({
+                    "label": lname,
+                    "subject": subject,
+                    "time": t,
+                    "value": data[j, t]
+                })
+    df = pd.DataFrame(rows)
+    df.to_csv(FIGURES_DIR / "TM" / "data" / f'{network}_subreg_merged_pa_tr.csv', index=False, sep=",")
