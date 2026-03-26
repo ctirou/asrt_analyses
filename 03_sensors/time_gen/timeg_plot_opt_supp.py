613514569124d93a9d63c7f8bd20938eb072b441
ctirou <102290014+ctirou@users.noreply.github.com>
2026-03-26 11:54:10 +0100

    better opt_plot and table export 🎢

--- 03_sensors/time_gen/timeg_plot_opt_supp.py
+++ 03_sensors/time_gen/timeg_plot_opt_supp.py
@@ -62,12 +62,13 @@ crdm = "#FF718B"
 
 plt.rcParams.update({'font.size': 12, 'font.family': 'serif', 'font.serif': 'Arial'})
 
-fig, axes = plt.subplot_mosaic([['top', 'right'], ['bot', 'right']],
+fig, axes = plt.subplot_mosaic([['top', 'right_top'], ['bot', 'right_bot']],
                                figsize=(10, 5), layout='tight')
-ax1a, ax1b, ax2 = axes['top'], axes['bot'], axes['right']
+ax1a, ax1b, ax2, ax3 = axes['top'], axes['bot'], axes['right_top'], axes['right_bot']
 ax1b.sharex(ax1a)
+ax3.sharex(ax2)
 
-for ax in (ax1a, ax1b, ax2):
+for ax in (ax1a, ax1b, ax2, ax3):
     ax.spines['top'].set_visible(False)
     ax.spines['right'].set_visible(False)
 
@@ -80,8 +81,8 @@ ax1a.fill_between(times[win], pats_mean.mean(0) - pats_mean.std(0) / np.sqrt(n),
 ax1a.plot(times[win], rands_mean.mean(0), label="Random", color=crdm)
 ax1a.fill_between(times[win], rands_mean.mean(0) - rands_mean.std(0) / np.sqrt(n),
                   rands_mean.mean(0) + rands_mean.std(0) / np.sqrt(n), alpha=0.2, color=crdm)
-ax1a.set_ylabel("Accuracy (%)")
-ax1a.set_title("Decoding")
+ax1a.set_ylabel("Decoding accuracy (%)\n")
+ax1a.set_title("Single trial level")
 ax1a.legend(fontsize=9, frameon=False)
 
 # bottom left: contrast
@@ -89,7 +90,6 @@ max1 = np.argmax(pats_mean.mean(0)[times[win] <= 0.2])
 min2 = np.argmin(contrast_mean.mean(0)[times[win] > 0.2])
 hw = 3  # half-window size for min2
 idx_02 = np.sum(times[win] <= 0.2) - 1 # last index at 0.2s
-
 ax1b.axvspan(0, 0.2, color="lightgray", alpha=0.2)
 ax1b.axhline(0, color="grey", linestyle="-", alpha=0.5)
 ax1b.plot(times[win], contrast_mean.mean(0), color="g")
@@ -99,8 +99,7 @@ ax1b.axvspan(times[win][max1], times[win][idx_02], facecolor="b", alpha=0.2)
 ax1b.axvspan(times[win][max(0, min2 + np.sum(times[win] <= 0.2) - hw)],
              times[win][min2 + np.sum(times[win] <= 0.2) + hw], facecolor="r", alpha=0.2)
 ax1b.set_xlabel("Time (s)")
-ax1b.set_ylabel("Diff. in accuracy (a.u.)")
-ax1b.set_title("Contrast")
+ax1b.set_ylabel("Diff. in accuracy (%)")
 
 # right: block level
 max1_idx = max1
@@ -114,30 +113,45 @@ ax2.plot(blocks, sharp.mean(0), color="b", alpha=0.3)
 ax2.plot(blocks, gaussian_filter1d(sharp.mean(0), sigma), color="b", label="Sharpening")
 ax2.plot(blocks, damp.mean(0), color="r", alpha=0.3)
 ax2.plot(blocks, gaussian_filter1d(damp.mean(0), sigma), color="r", label="Dampening")
-ax2.set_xlabel("Block")
-ax2.set_ylabel("Diff. in accuracy (a.u.)")
+ax2.set_ylabel("Diff. in accuracy (%)\n")
 ax2.set_xticks(blocks[::2])
 ax2.set_xticks(blocks, minor=True)
 ax2.grid(which='major', alpha=0.4, linestyle=':')
 ax2.grid(which='minor', alpha=0.15, linestyle=':')
-ax2.legend(fontsize=9, frameon=False, loc='lower right')
-ax2.set_title("Block level effects")
+ax2.legend(fontsize=9, frameon=False, loc='upper left')
+ax2.set_title("Block level")
+
+# bottom right: contrast (sharpening - dampening)
+contrast_blocks = sharp - damp
+ax3.axvspan(1, 3, color="lightgray", alpha=0.2)
+ax3.axhline(0, color="grey", linestyle="-", alpha=0.5)
+ax3.plot(blocks, contrast_blocks.mean(0), color="purple", alpha=0.3)
+ax3.plot(blocks, gaussian_filter1d(contrast_blocks.mean(0), sigma), color="purple")
+ax3.fill_between(blocks,
+                 contrast_blocks.mean(0) - contrast_blocks.std(0) / np.sqrt(n),
+                 contrast_blocks.mean(0) + contrast_blocks.std(0) / np.sqrt(n),
+                 alpha=0.15, color="purple")
+ax3.set_xlabel("Block")
+ax3.set_ylabel("Diff. in accuracy (%)")
+ax3.set_xticks(blocks[::2])
+ax3.set_xticks(blocks, minor=True)
+ax3.grid(which='major', alpha=0.4, linestyle=':')
+ax3.grid(which='minor', alpha=0.15, linestyle=':')
 
 plt.savefig(figure_dir / "timeg_opt_block_supp.pdf", dpi=300, transparent=True)
 plt.close()
 
-# export table of block-level effects
-for effect, data, fname in [
-    ("sharpening", sharp, "timeg_sensors_sharp_br.csv"),
-    ("dampening", damp, "timeg_sensors_damp_br.csv"),
-]:
-    rows = list()
-    for i, subject in enumerate(subjects):
-        for block in range(data.shape[1]):
-            rows.append({
-                "subject": subject,
-                "block": block + 1,
-                "value": data[i, block]
-            })
-    df = pd.DataFrame(rows)
-    df.to_csv(FIGURES_DIR / "TM" / "data" / fname, index=False, sep=",")
+# export table of contrast (sharpening - dampening) at trial and block level
+rows = list()
+for i, subject in enumerate(subjects):
+    for t, time in enumerate(times[win]):
+        rows.append({"subject": subject, "time": time, "value": contrast_mean[i, t]})
+df = pd.DataFrame(rows)
+df.to_csv(FIGURES_DIR / "TM" / "data" / "timeg_sensors_contrast_opt_tr.csv", index=False, sep=",")
+
+rows = list()
+for i, subject in enumerate(subjects):
+    for block in range(contrast_blocks.shape[1]):
+        rows.append({"subject": subject, "time": block + 1, "value": contrast_blocks[i, block]})
+df = pd.DataFrame(rows)
+df.to_csv(FIGURES_DIR / "TM" / "data" / "timeg_sensors_contrast_opt_br.csv", index=False, sep=",")
